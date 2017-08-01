849ba9b23747765e5d0d8d8351d15cd3f8d9f237
AwsIdris <awsidris@gmail.com>
2017-08-01 12:56:08 +0200

    SQL HomeWork Week One

--- examples/basic_mysql_connection/index.js
+++ examples/basic_mysql_connection/index.js
@@ -4,17 +4,28 @@ var mysql = require('mysql');
 var config = JSON.parse(fs.readFileSync("config-secret.json"))
 
 var connection = mysql.createConnection({
-	host: config.host,
+	 host: config.host,
 	user: config.user,
 	password: config.password,
 	port: config.port,
-    database: config.database
+  database: config.database
 });
 
 connection.connect();
+const sqlStatments=['SELECT COUNT(*) as "Total No of Todos" FROM todos',
+					'SELECT * FROM todos WHERE Done=1',
+					'SELECT * FROM todos WHERE Done=0',
+					'SELECT * FROM todos GROUP BY Due DESC',
+					'SELECT * FROM todos WHERE Id=(SELECT MAX(Id) FROM todos) ',
+					'SELECT * FROM todos WHERE Name like "%databases%"',
 
-connection.query('SELECT * FROM todos', function (error, results, fields) {
+				  ]
+let sql = sqlStatments.map(function(value) {
+  connection.query(value, function(error, results, fields) {
     console.log(results);
-});
+	console.log("=========================================================");
+  });
 
+
+})
 connection.end();
