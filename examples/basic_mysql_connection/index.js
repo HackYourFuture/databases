f634a7058bec44ac9e47c15295be811faca1cd62
Odai-kakhi <odaikakhi@gmail.com>
2017-08-02 21:48:45 +0200

    HomeWork DataBase

--- examples/basic_mysql_connection/index.js
+++ examples/basic_mysql_connection/index.js
@@ -4,17 +4,27 @@ var mysql = require('mysql');
 var config = JSON.parse(fs.readFileSync("config-secret.json"))
 
 var connection = mysql.createConnection({
-	host: config.host,
+	 host: config.host,
 	user: config.user,
 	password: config.password,
 	port: config.port,
-    database: config.database
+  database: config.database
 });
 
 connection.connect();
+const sqlStatments=['SELECT COUNT(*) as "Total No of Todos" FROM todos',
+					'SELECT * FROM todos WHERE Done',
+					'SELECT * FROM todos WHERE NOT Done',	
+					'SELECT * FROM todos GROUP BY Id DESC',
+					'SELECT * FROM todos  ORDER BY id DESC LIMIT 1',
+					'SELECT * FROM todos WHERE Name like "%databases%"',
 
-connection.query('SELECT * FROM todos', function (error, results, fields) {
+				  ]
+let sql = sqlStatments.map(function(value) {
+  connection.query(value, function(error, results, fields) {
     console.log(results);
-});
+  });
 
+
+})
 connection.end();
