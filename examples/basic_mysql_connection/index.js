2c5fa53116b1aee20394f31db6b150b3063ba7d7
Jad-kad <kaddour.jad@gmail.com>
2017-08-10 14:30:19 +0200

    database-week2

--- examples/basic_mysql_connection/index.js
+++ examples/basic_mysql_connection/index.js
@@ -17,7 +17,6 @@ let sql = ['SELECT COUNT(*) as todo-Items FROM todos',
 			'SELECT * FROM todos where !done',
 			'SELECT * FROM todos ORDER BY Id DESC',
 			'SELECT * FROM todos ORDER BY Id DESC LIMIT 1',
-<<<<<<< HEAD
 			'SELECT * FROM todos WHERE Name like "%databases%"',
 			'INSERT INTO todos (Name,Done) VALUES ("go climbing",0)',
 			'UPDATE todos SET Due="2017-9-10" WHERE Name="repair my bike"',
@@ -32,13 +31,3 @@ connection.query('SELECT * FROM todos', function (error, results, fields) {
 	});
 })
 connection.end();
-=======
-			'SELECT * FROM todos WHERE Name like "%databases%"'
-			]
-let mySql = sql.map(function(value) {
-	connection.query('SELECT * FROM todos', function (error, results, fields) {
-    console.log(results);
-	});
-})
-connection.end();
->>>>>>> ae33bbd9e3be04ba52f6bc3ac316c56091916066
