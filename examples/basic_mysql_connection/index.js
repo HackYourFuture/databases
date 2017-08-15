2a9d2938dcbd68f0c493681df6c2ecda6bb95476
massoodbarsa <amir.shakiba1980@@email.com>
2017-08-15 14:53:27 +0200

    week3.1

--- examples/basic_mysql_connection/index.js
+++ examples/basic_mysql_connection/index.js
@@ -12,7 +12,7 @@ var connection = mysql.createConnection({
 });
 
 connection.connect();
-let query=['INSERT INTO todos(id,done) VALUES '+mysql.escape(18,0),
+let query=['INSERT INTO todos(id,done) VALUES ('+mysql.escape(18,0)),
            'UPDATE todos SET name="ali",due="2019-08-31" WHERE id='+mysql.escape(5),
            'DELETE FROM todos WHERE NAME='+mysql.escape("ali"),
 					 'UPDATE todos SET done=1 where id='+mysql.escape(9),
