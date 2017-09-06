ce1d1014908a5b21b675345838d607d38de23bb8
gmamar <gmamar1@hotmail.com>
2017-09-07 01:26:11 +0200

    sum cleanup

--- examples/basic_mysql_connection/index.js
+++ examples/basic_mysql_connection/index.js
@@ -2,105 +2,122 @@ var fs = require('fs');
 var mysql = require('mysql');
 var prompt = require('prompt');
 
-var config = JSON.parse(fs.readFileSync("config-secret.json"))
+var config = JSON.parse(fs.readFileSync('config-secret.json'));
 
 var connection = mysql.createConnection({
-	host: config.host,
-	user: config.user,
-	password: config.password,
-	port: config.port,
+    host: config.host,
+    user: config.user,
+    password: config.password,
+    port: config.port,
     database: config.database
 });
 
-connection.connect();
+const option = process.argv.slice(2);
 
-const options = process.argv.slice(2);
+switch (option[0]){
+    case 'list':
+        listData();
+        break;
+    case 'add':
+        addNewEntry();
+        break;
+    case 'update':
+        updateData();
+        break;
+    case 'delete':
+        DeleteData();
+        break;
+    default:
+        console.log(`Please use 'list' 'add' 'update' 'delete' to edit DataBase`);
+         break;                       
+    
+};
 
-switch(options[0]){
-	case 'add':
-		addNewEntry();
-		break;
-	case 'list':
-		listTodos();
-		break;
-	default:
-		console.log('No Entry');
+function listData(){
+    var query = 'SELECT * FROM todos';
+    sendQuery(query);
 }
 
 function addNewEntry(){
-	
-	var ItemsToAdd=[];
-	prompt.start();
-	prompt.get(['Name', 'Due'], function (err, result) {
-		ItemsToAdd = [result.Name, result.Due];
-		console.log(ItemsToAdd);
-		addQurey(ItemsToAdd);
-	  });
+    console.log (`\nEnter new todo item and the date, to enter curent date and time just enter "now"\n`)
+    prompt.start();
+    prompt.get(['Name', 'Due'], (err, res) => {
+        if (err){
+            console.log(err);
+        } else {
+            
+            var escName = mysql.escape(res.Name);
+            var escDue = mysql.escape(res.Due);
+            if (escDue == "'Now'"|| escDue == "'now'"){
+                escDue = "NOW()";
+            }
+            const query = `INSERT INTO
+                            todos
+                            (
+                                Name,
+                                Due
+                            )
+                            VALUES(
+                                ${escName},
+                                ${escDue}
+                            )`;
+            console.log(query);                
+            sendQuery(query);
+        }
+    });
+    
+}
 
-	function addQurey(addItems){
-		console.log(addItems);
-		connection.query(`
-			INSERT INTO
-				todos
-				(
-					Name,
-					Due
-				)
-				VALUES(
-					?,
-					?
-				)
-			`,
-			[
-				addItems[0],
-				addItems[1]
-			],
-			function (error, results){
-				if(error) {
-					//Handel error
-					console.log(error);
-				}else{
-					//Process results
-					console.log(results);
-				}
-			}
-			
-		)
+function updateData(){ 
+    console.log(`\n Enter the row ID then enter colume name "Done", "Name", "StatusId", "Due" 
+    then enter new value.                 0-1    value     1-2-3     YYYY-MM-DD HH:MM:SS. \n`);
+    prompt.start();
+    prompt.get(['ID', 'Colume_name', 'New_value'], (err, res) => {
+        if(err) {
+            throw err
+        } else {
+            var escId = mysql.escape(res.ID);
+            var escColName = mysql.escape(res.Colume_name);
+            var escNewValue = mysql.escape(res.New_value);
+            const query = `UPDATE
+                                todos
+                            SET
+                                ${res.Colume_name} = ${escNewValue}
+                            WHERE
+                                id = ${escId} `;
+            sendQuery(query);
+        }
+    });
+}
 
-	};
-	connection.end();
-};
-function listTodos(){
-	
-	console.log('Done', options[1]);
-	connection.query(`
-		SELECT * FROM
-			todos
-			`,
-	
-	function (error, results){
-		if(error) {
-			//Handel error
-			console.log(error);
-		}else{
-			//Process results
-			console.log(results);
-			
-		}
-	}
-	
-)
-connection.end();
-};
+function DeleteData(){
+    console.log(`\nEnter row Id number to delete \n`);
+    prompt.start();
+    prompt.get(['ID'],(err, res) => {
+        if (err){
+            throw err;
+        } else {
+            var escId = mysql.escape(res.ID);
+            const query = `DELETE FROM 
+                                todos
+                            WHERE
+                                id = ${escId}`;
+            sendQuery(query);
+        }
+    })
+}
 
-// connection.query(`SELECT * FROM todos`,function (error, results) {
-// 	if (error) {
-// 		console.log(error);
-// 	} else {
-// 		console.log(results);
-// 	}
-// } )
-
-
-
-
+function sendQuery(queryData){
+    connection.connect;
+    connection.query(
+        queryData,
+        (err, result) => {
+            if(err){
+                console.log(err);
+            } else {
+                console.log(result);
+            }
+        }
+    )
+    connection.end();
+}
