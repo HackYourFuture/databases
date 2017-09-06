768f3d580accf438a864e7eac0b084bda000107d
gmamar <gmamar1@hotmail.com>
2017-09-07 01:25:08 +0200

    end of home work

--- examples/basic_mysql_connection/index.js
+++ examples/basic_mysql_connection/index.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var mysql = require('mysql');
+var prompt = require('prompt');
 
 var config = JSON.parse(fs.readFileSync("config-secret.json"))
 
@@ -12,36 +13,94 @@ var connection = mysql.createConnection({
 });
 
 connection.connect();
-//my input
 
-const input= "New Name";
-connection.query(`
-	INSERT INTO
-		todos
-		(
-			Name
-		)
-		VALUES(
-			?
-		)
-`,
-[
-	input
-],
-function (error, results){
-	if(error) {
-		//Handel error
-		console.log(error);
-	}else{
-		//Process results
-		console.log(results);
-	}
+const options = process.argv.slice(2);
+
+switch(options[0]){
+	case 'add':
+		addNewEntry();
+		break;
+	case 'list':
+		listTodos();
+		break;
+	default:
+		console.log('No Entry');
 }
-);
 
-connection.query('SELECT * FROM todos', function (error, results, fields) {
-	console.log(results);
-	if (error) console.log(error);
-});
+function addNewEntry(){
+	
+	var ItemsToAdd=[];
+	prompt.start();
+	prompt.get(['Name', 'Due'], function (err, result) {
+		ItemsToAdd = [result.Name, result.Due];
+		console.log(ItemsToAdd);
+		addQurey(ItemsToAdd);
+	  });
 
+	function addQurey(addItems){
+		console.log(addItems);
+		connection.query(`
+			INSERT INTO
+				todos
+				(
+					Name,
+					Due
+				)
+				VALUES(
+					?,
+					?
+				)
+			`,
+			[
+				addItems[0],
+				addItems[1]
+			],
+			function (error, results){
+				if(error) {
+					//Handel error
+					console.log(error);
+				}else{
+					//Process results
+					console.log(results);
+				}
+			}
+			
+		)
+
+	};
+	connection.end();
+};
+function listTodos(){
+	
+	console.log('Done', options[1]);
+	connection.query(`
+		SELECT * FROM
+			todos
+			`,
+	
+	function (error, results){
+		if(error) {
+			//Handel error
+			console.log(error);
+		}else{
+			//Process results
+			console.log(results);
+			
+		}
+	}
+	
+)
 connection.end();
+};
+
+// connection.query(`SELECT * FROM todos`,function (error, results) {
+// 	if (error) {
+// 		console.log(error);
+// 	} else {
+// 		console.log(results);
+// 	}
+// } )
+
+
+
+
