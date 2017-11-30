4d3dfd449d1a3743c65964f79adc8969fbec6858
pesh12 <ekocibar@hotmail.com>
2017-11-30 16:33:20 +0100

    added new features for creating users

--- Week2/todo_app/user.sql
+++ Week2/todo_app/user.sql
@@ -1,3 +1,8 @@
+--CREATE USER AND GRANT PERMISSIONS--
 CREATE USER 'user'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON todo_app.* TO 'user'@'localhost'
+GRANT ALL PRIVILEGES ON todo_app.* TO 'todo_user'@'localhost';
+--OR--
+GRANT ALL PRIVILEGES ON todo_app.* To 'todo_user'@'localhost' IDENTIFIED BY '123';
 
+--DELETE USER--
+DROP USER 'todo_user'@'localhost';
