6c936b4edd683dd35883d36959491d0533ad103b
Martin Clemmensen <clemme@jir.dk>
2018-02-06 19:47:08 +0100

    Basic node.js connection example

--- lesson2/node_mysql_connection/index.js
+++ lesson2/node_mysql_connection/index.js
@@ -4,16 +4,16 @@ var mysql = require('mysql');
 var config = JSON.parse(fs.readFileSync("config-secret.json"))
 
 var connection = mysql.createConnection({
-	host: config.host,
-	user: config.user,
-	password: config.password,
-	port: config.port,
+    host: config.host,
+    user: config.user,
+    password: config.password,
+    port: config.port,
     database: config.database
 });
 
 connection.connect();
 
-connection.query('SELECT * FROM todos', function (error, results, fields) {
+connection.query('SELECT * FROM user', function(error, results, fields) {
     console.log(results);
 });
 
