f78fe9ed02bc9e4fc28fd4625930ccf17c56be0d
Dumie1 <dumiedumara@gmail.com>
2018-07-27 17:35:19 +0200

    created a new user for todo_app database

--- Week2/todo_app/program.js
+++ Week2/todo_app/program.js
@@ -64,21 +64,45 @@ class TodoModel {
 
     tagTodoItem(todoItemId, tagId, callback) {
         // Write code and query add a tag to a TODO item
+        dbConnection.query(`INSERT INTO todo_item_tag VALUES = ?) 
+        ${ this.dbConnection.escape(todoItemId)}, ${this.dbConnection.escape(tagId)}`, function (err, results, fields) {
+                if (err) {
+                    callback(err);
+                    return;
+                }
+                callback(null, results);
+            });
     }
         
     untagTodoItem(todoItemId, tagId, callback) {
         // Write code and query remove a tag from a TODO item
+        dbConnection.query(`DELETE FROM todo_item_tag WHERE todo_item_id = ? AND tag_id = ? 
+        ${this.dbConnection.escape(todoItemId)}, ${this.dbConnection.escape(tagId)}`, function (err, results, fields) {
+                if (err) {
+                    callback(err);
+                    return;
+                }
+                callback(null, results);
+            });
     }
 
     markCompleted(todoItemId, callback) {
         // Write code to mark a TODO item as completed
+        dbConnection.query(`UPDATE todo_items SET is_completed = true
+        WHERE id = ? ${this.dbConnection.escape(todoItemId)}`, function (err, results, fields) {
+                if (err) {
+                    callback(err);
+                    return;
+                }
+                callback(null, results);
+            });
+         }
     }
-}
 
 const dbConnection = mysql.createConnection({
     host     : 'localhost',
-    user     : 'root',
-    password : '',
+    user     : 'dumie',
+    password : '1234',
     database : 'todo_app'
 });
 
@@ -99,3 +123,31 @@ dbConnection.connect(function(err) {
         console.log("existing todo items:", todoItems);
     });
 });
+
+dbConnection.end();
+
+// === BELOW ARE STEPS AND COMMANDS I USED TO CREATE A NEW USER IN MYSQL.===
+
+/*
+1. MariaDB [(none)]> use todo_app;
+
+2. MariaDB [todo_app]> CREATE USER 'dumie'@'localhost' IDENTIFIED BY '1234';
+
+3. MariaDB [todo_app]> GRANT ALL ON todo_app. * TO 'dumie'@'localhost';
+
+4. MariaDB [todo_app]> FLUSH PRIVILEGES;
+
+5. MariaDB [todo_app]> exit
+
+6. hackyourfuture@hackyourfuture-Latitude-E5430-non-vPro:~/HackYourFuture/databases/Week2/todo_app$ mysql -u dumie -p1234
+
+7. MariaDB [(none)]> show databases;
++--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| todo_app           |
++--------------------+
+2 rows in set (0.00 sec)
+
+*/
