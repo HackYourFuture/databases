fc0e07b3c5e5c29fb74e3ce2b7e3bbfc7f65c639
martamatos <marta.ra.matos@gmail.com>
2018-10-02 19:32:47 +0200

    Updated homework specification for lesson2

---
+++ lesson2/homework.js
@@ -0,0 +1,30 @@
+
+
+const addNewTask = function(id, title, description, created, updated, dueDate, statusID, userID) {
+  
+};
+
+const changeTaskTitle = function(taskID, newTitle) {
+  
+};
+
+const changeTaskDueDate = function(taskID, newDueDate) {
+  
+};
+
+const changeTaskStatus = function(taskID, newStatus) {
+  
+};
+
+const markTaskAsCompleted = function(taskID, newStatus) {
+  
+};
+
+const deleteTask = function(taskID, taskID) {
+  
+};
+
+const deleteUser = function(taskID, userID) {
+  
+};
+
