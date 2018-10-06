da07a348693643cc30743cb5c89c0e76194b9213
Donald Zodeh <donald_leb@hotmail.com>
2018-10-07 01:29:55 +0200

    Donald's homework week1 fixed

--- Week1/homework/to_do.sql
+++ Week1/homework/to_do.sql
@@ -2,27 +2,31 @@ DROP database IF EXISTS to_do;
 CREATE database to_do;
 USE to_do;
 
-CREATE TABLE Users (
-user_Id INT NOT NULL,
+CREATE TABLE users (
+user_id INT PRIMARY KEY AUTO_INCREMENT,
 user_name varchar(30) NOT NULL,
-user_language varchar(10) NOT NULL,
-PRIMARY KEY (user_Id)
+user_language varchar(10) NOT NULL
 );
 
-CREATE TABLE Tasks (
-task_Id INT NOT NULL,
-user_task INT NOT NULL,
-task_name varchar(30) NOT NULL,
-task_status varchar(10) NOT NULL,
-PRIMARY KEY (task_Id),
-FOREIGN KEY (user_task) REFERENCES Users(user_Id)
+CREATE TABLE tasks (
+task_id INT PRIMARY KEY AUTO_INCREMENT,
+task_title varchar(30) NOT NULL,
+task_status varchar(30) NOT NULL,
+users INT NOT NULL,
+tag INT NOT NULL,
+FOREIGN KEY (users) REFERENCES users(user_id),
+FOREIGN KEY (tag) REFERENCES tag(tag_id)
 );
 
-INSERT INTO Users (user_Id, user_name, user_language) VALUES (1, 'Donald', 'Arabic');
-INSERT INTO Users (user_Id, user_name, user_language) VALUES (2, 'Ward', 'Arminian');
-INSERT INTO Users (user_Id, user_name, user_language) VALUES (3, 'Karam', 'Arabic');
+CREATE TABLE tag (
+  tag_id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) UNIQUE NOT NULL
+);
 
-INSERT INTO Tasks (task_Id, user_task, task_name, task_status) VALUES (1, 2, 'make salad', 'done');
-INSERT INTO Tasks (task_Id, user_task, task_name, task_status) VALUES (2, 1, 'BBQ', 'done');
-INSERT INTO Tasks (task_Id, user_task, task_name, task_status) VALUES (3, 3, 'eat', 'done');
+INSERT INTO users (user_id, user_name, user_language) VALUES (1, 'Donald', 'Arabic');
+INSERT INTO users (user_id, user_name, user_language) VALUES (2, 'Ward', 'Arminian');
+INSERT INTO users (user_id, user_name, user_language) VALUES (3, 'Karam', 'Arabic');
 
+INSERT INTO Tasks (task_id, users, task_title, task_status) VALUES (1, 1, 'make salad', 'done');
+INSERT INTO Tasks (task_id, users, task_title, task_status) VALUES (2, 2, 'BBQ', 'done');
+INSERT INTO Tasks (task_id, users, task_title, task_status) VALUES (3, 3, 'eat', 'done');
--- Week2/todo_app/program.js
+++ Week2/todo_app/program.js
@@ -11,8 +11,8 @@ class TodoModel {
     // Loads all the TODOs in the database
     load(callback) {
         const selectTodoItems = "SELECT * FROM todo_items";
-        this.dbConnection.query(selectTodoItems, function(err, results, fields) {
-            if(err) {
+        this.dbConnection.query(selectTodoItems, function (err, results, fields) {
+            if (err) {
                 callback(err);
                 return;
             }
@@ -36,7 +36,7 @@ class TodoModel {
     tagTodoItem(todoItemId, tagId, callback) {
         // Write code and query add a tag to a TODO item
     }
-        
+
     untagTodoItem(todoItemId, tagId, callback) {
         // Write code and query remove a tag from a TODO item
     }
@@ -47,13 +47,13 @@ class TodoModel {
 }
 
 const dbConnection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'todo_app'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'todo_app'
 });
 
-dbConnection.connect(function(err) {
+dbConnection.connect(function (err) {
     if (err != null) {
         console.error('error connecting: ' + err.stack);
         return;
@@ -62,8 +62,8 @@ dbConnection.connect(function(err) {
     console.log('connected as id ' + dbConnection.threadId);
 
     const todoModel = new TodoModel(dbConnection);
-    todoModel.load(function(err, todoItems) {
-        if(err) {
+    todoModel.load(function (err, todoItems) {
+        if (err) {
             console.log("error loading TODO items:", err);
         }
 
