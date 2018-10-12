42ac70af060df96d4d1f8e9ce53cf0acce3311bb
Ozgur Aslan <alpozguraslan@gmail.com>
2018-10-12 18:59:18 +0200

    the week2 assigment

--- Week2/todo_app/db.js
+++ Week2/todo_app/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
     host     : 'localhost',
-    user     : 'root',
+    user     : 'user',
     password : 'password',
     database : 'todo_app'
   });
