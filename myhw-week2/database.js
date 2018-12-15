c1647d71952802d6d38d943d5d06e5ee9ac1d80c
RimSebai <40184200+RimSebai@users.noreply.github.com>
2018-12-15 13:26:03 +0100

    Update database.js
    Hi Lucas, thanks for your feedback!
    I did change it

--- myhw-week2/database.js
+++ myhw-week2/database.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql');
+const util = require('util');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'hyfuser',
@@ -6,38 +7,55 @@ const connection = mysql.createConnection({
   database: 'new_world'
 });
 
-connection.connect();
-//Part 1 - 1
-let country_name = 'Syria';
-let capitalOfCountry = 'select country.name, city.name from city inner join country on city.id = country.capital where country.name = ' + connection.escape(country_name);
 
-//Part 1 - 2
-let country_region = 'Western Europe';
-let findingSpokenLanguages = 'select countryLanguage.language, country.region from country join countryLanguage on country.Code = countryLanguage.countryCode where country.region =' + connection.escape(country_region);
+const execQuery = util.promisify(connection.query.bind(connection));
 
-//Part 1 - 3
-let country_language = 'Dutch';
-let findingNumberOfCities = 'select count(*) as numberOfCities from city join countryLanguage on city.countryCode = countryLanguage.countryCode where language = ' + connection.escape(country_language);
+async function seedDatabase() {
 
-//Part 1 - 4
-let display_countries = 'select name,region,language,count(*) from country join countryLanguage on code = countryCode where concat(language - region) = concat(language - region) and IsOfficial = 'T' group by region,language having count(*) > 1 order by language'
-//part 1 - 5 
-let list_continents = 'select count(countryLanguage.language), country.continent from country join countryLanguage on country.code = countryLanguage.countryCode group by country.continent'
 
-let world = [capitalOfCountry, findingSpokenLanguages, findingNumberOfCities, display_countries, list_continents];
+  //Part 1 - 1
+  let country_name = 'Syria';
+  let capitalOfCountry = 'select country.name, city.name from city inner join country on city.id = country.capital where country.name = ' + connection.escape(country_name);
 
-world.forEach(function (el) {
-  connection.query(el, function (error, result) {
-    if (error) {
-      throw error;
-    }
-    console.log(result);
-  })
-});
+  //Part 1 - 2
+  let country_region = 'Western Europe';
+  let findingSpokenLanguages = 'select countryLanguage.language, country.region from country join countryLanguage on country.Code = countryLanguage.countryCode where country.region =' + connection.escape(country_region);
 
-connection.end();
+  //Part 1 - 3
+  let country_language = 'Dutch';
+  let findingNumberOfCities = 'select count(*) as numberOfCities from city join countryLanguage on city.countryCode = countryLanguage.countryCode where language = ' + connection.escape(country_language);
 
-// part 2 
+  //Part 1 - 4
+  let display_countries = "select name, region from country as a join countryLanguage as b on a.code = b.countryCode where b.Language = 'French' and b.IsOfficial = 'T' and region in (select region from country as a join countryLanguage as b on a.code = b.countryCode where b.Language = 'French' and b.IsOfficial = 'T' group by region having COUNT(*) > 1 )"
+
+  //part 1 - 5 
+  let list_continents = 'select count(countryLanguage.language), country.continent from country join countryLanguage on country.code = countryLanguage.countryCode group by country.continent'
+
+  let world = [capitalOfCountry, findingSpokenLanguages, findingNumberOfCities, display_countries, list_continents];
+  connection.connect();
+
+
+  try {
+    let number1 = await execQuery(capitalOfCountry);
+    console.log(number1);
+    let number2 = await execQuery(findingSpokenLanguages);
+    console.log(number2);
+    let number3 = await execQuery(findingNumberOfCities);
+    console.log(number3);
+    let number4 = await execQuery(display_countries);
+    console.log(number4);
+    let number5 = await execQuery(list_continents);
+    console.log(number5);
+  } catch (error) {
+    console.error(error);
+  }
+
+  connection.end();
+}
+seedDatabase();
+
+
+// part 2
 
 //CREATE TRIGGER my_trigger  
 //BEFORE INSERT ON countryLanguage 
