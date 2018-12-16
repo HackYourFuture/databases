4c03305fb5dee4aa2ac7cd7ed78ba6c940174aef
RimSebai <40184200+RimSebai@users.noreply.github.com>
2018-12-16 03:37:09 +0100

    Update database2.js
    I hope it is all fine now!

--- Week2/database2.js
+++ Week2/database2.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 const util = require('util');
+const process = require('process')
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'hyfuser',
@@ -16,39 +17,50 @@ async function seedDatabase() {
   //Part 1 - 1
 
   function capitalOfCountry(country_name) {
-    return 'select country.name, city.name from city inner join country on city.id = country.capital where country.name = ' + connection.escape(country_name);
+    return 'select country.name, city.name from city inner join country on id = capital where country.name = ' + connection.escape(country_name);
   }
   //Part 1 - 2
   function findingSpokenLanguages(country_region) {
-    return 'select countryLanguage.language, country.region from country join countryLanguage on country.Code = countryLanguage.countryCode where country.region =' + connection.escape(country_region);
+    return 'select language, region from country join countryLanguage on Code = countryCode where region =' + connection.escape(country_region);
   }
 
   //Part 1 - 3
   function findingNumberOfCities(country_language) {
-    return 'select count(*) as numberOfCities from city join countryLanguage on city.countryCode = countryLanguage.countryCode where language = ' + connection.escape(country_language);
+    return 'select count(*) as numberOfCities from city a join countryLanguage b on a.countryCode = b.countryCode where language = ' + connection.escape(country_language);
   }
   //Part 1 - 4
   function display_countries() {
-    return "select name, region from country as a join countryLanguage as b on a.code = b.countryCode where b.Language = 'French' and b.IsOfficial = 'T' and region in (select region from country as a join countryLanguage as b on a.code = b.countryCode where b.Language = 'French' and b.IsOfficial = 'T' group by region having COUNT(*) > 1 )"
+    return "select name,region,language,count(*) from country a join countryLanguage b on code = countryCode where IsOfficial = 'T' group by region having count(*) > 1"
   }
   //part 1 - 5 
   function list_continents() {
-    return 'select count(countryLanguage.language), country.continent from country join countryLanguage on country.code = countryLanguage.countryCode group by country.continent'
+    return 'select count(language),continent from country join countryLanguage on code = countryCode group by continent'
   }
 
   connection.connect();
 
   try {
-    let number1 = await execQuery(capitalOfCountry('Syria'));
-    console.log(number1);
-    let number2 = await execQuery(findingSpokenLanguages('North America'));
-    console.log(number2);
-    let number3 = await execQuery(findingNumberOfCities('Dutch'));
-    console.log(number3);
-    let number4 = await execQuery(display_countries());
-    console.log(number4);
-    let number5 = await execQuery(list_continents());
-    console.log(number5);
+    switch (process.argv[2]) {
+      case 'number1':
+        let number1 = await execQuery(capitalOfCountry(process.argv[3]));
+        console.log(number1);
+        break;
+      case 'number2':
+        let number2 = await execQuery(findingSpokenLanguages(process.argv[3]));
+        console.log(number2);
+        break;
+      case 'number3':
+        let number3 = await execQuery(findingNumberOfCities(process.argv[3]));
+        console.log(number3);
+        break;
+      case 'number4':
+        let number4 = await execQuery(display_countries());
+        console.log(number4);
+        break;
+      case 'number5':
+        let number5 = await execQuery(list_continents());
+        console.log(number5);
+    }
   } catch (error) {
     console.error(error);
   }
@@ -72,5 +84,3 @@ seedDatabase();
 //////the test insert statements////////
 
 // insert into countryLanguage values('USA', 'sansakrity', 'f', 5.3);
-
-
