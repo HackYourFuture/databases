4501a3bd321f375b3e2f9e22150979fe8b039b8b
Yaacob <yyaacob0@gmail.com>
2019-02-16 17:47:56 +0100

    for the real PR

--- Week3/server.js
+++ Week3/server.js
@@ -1,45 +1,45 @@
 'use strict';
 
-const bodyParser = require('body-parser');
 const express = require('express');
-const db = require('./queries.js');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+const query = require('./queries.js');
 
 app.use(bodyParser.json());
 
 app.post('/createUser/:id', (request, response) => {
-  db.createUser(request.params.userId, request.body.userName);
+  query.createUser(request.params.userId, request.body.userName);
   response.send('user added');
 });
 
 app.post('/createList/:id', (request, response) => {
-  db.createList(request.params.listId, request.body.listName);
+  query.createList(request.params.listId, request.body.listName);
   response.send('new list is added');
 });
 
 app.post('/createItem/:id', (request, response) => {
-  db.createItem(request.params.itemId, request.body.description);
+  query.createItem(request.params.itemId, request.body.description);
   response.send('new item is added');
 });
 
 app.post('/createReminder/:id', (request, response) => {
-  db.createReminder(request.params.reminderId, request.body.reminder);
+  query.createReminder(request.params.reminderId, request.body.reminder);
   response.send('new reminder is added');
 });
 
 app.put('/updateItem/:id', (request, response) => {
-  db.updateItem(request.params.itemId, request.body.isCompleted);
+  query.updateItem(request.params.itemId, request.body.isCompleted);
   response.send('task is completed');
 });
 
 app.delete('/deleteItem/:id', (request, response) => {
-  db.deleteItem(request.params.itemId);
+  query.deleteItem(request.params.itemId);
   response.send('item is deleted');
 });
 
 app.delete('/deleteList/:id', (request, response) => {
-  db.deleteList(request.params.listId);
+  query.deleteList(request.params.listId);
   response.send('list is deleted');
 });
 
