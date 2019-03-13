5c24e0c8bb08349c58fe1ca9660ae618aff2a6da
YaseirAlkhwalda <43831623+YaseirAlkhwalda@users.noreply.github.com>
2019-03-13 04:11:53 +0100

    Update select.js

--- Week1/select.js
+++ Week1/select.js
@@ -14,17 +14,24 @@ const args = process.argv;
 args.splice(0, 2);
 const retrieve = argument => args.includes(argument);
 
-if (retrieve('1')) select_query = 'select DISTINCT * from countries where population > 8000000';
+
+if (retrieve('1'))
+  select_query =
+    'SELECT DISTINCT country_name, population FROM countries WHERE population > 8000000 ORDER BY population ASC';
 else if (retrieve('2'))
-  select_query = 'select DISTINCT * from countries where country_name like "%land%"';
+  select_query = 'SELECT DISTINCT country_name FROM countries WHERE country_name LIKE "%land%"';
 else if (retrieve('3'))
-  select_query = 'select DISTINCT * from cities where population between 500000 and 1000000';
+  select_query =
+    'SELECT DISTINCT city_name, population FROM cities WHERE population BETWEEN 500000 AND 1000000 ORDER BY population ASC';
 else if (retrieve('4'))
-  select_query = 'select DISTINCT * from countries where continent = "Europe"';
+  select_query =
+    'SELECT DISTINCT country_name, continent From countries WHERE continent = "Europe"';
 else if (retrieve('5'))
-  select_query = 'select DISTINCT * from countries order by surface_area DESC';
+  select_query =
+    'SELECT DISTINCT country_name, surface_area FROM countries ORDER BY surface_area DESC';
 else if (retrieve('help')) select_query = help;
 else select_query = help;
+
 
 connection.query(select_query, function(error, results, fields) {
   if (error) {
