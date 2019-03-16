592fd96ecbde3cafe73b5d344dbde2cda1ae7bd7
YaseirAlkhwalda <43831623+YaseirAlkhwalda@users.noreply.github.com>
2019-03-16 09:05:20 +0100

    update homework

--- Week1/create.js
+++ Week1/create.js
@@ -1,6 +1,8 @@
 const util = require('util');
 const mysql = require('mysql');
-const data = require('./data');
+const cities = require('nl-city');
+const Country = require('db-country');
+const countries = Country.findAll();
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -12,33 +14,39 @@ const connection = mysql.createConnection({
 const execQuery = util.promisify(connection.query.bind(connection));
 
 async function seedDatabase() {
+  const DROP_COUNTRIES_TABLE = `DROP TABLE IF EXISTS countries`;
+  const DROP_CITIES_TABLE = `DROP TABLE IF EXISTS cities`;
   const CREATE_DATABASE = `CREATE DATABASE IF NOT EXISTS world`;
   const CREATE_COUNTRIES_TABLE = `
     CREATE TABLE IF NOT EXISTS countries (
-      country_number INT,
-      country_name VARCHAR(50),
+      name_en VARCHAR(50),
       continent VARCHAR(30),
       population FlOAT,
-      surface_area FLOAT
+      area FLOAT
     );`;
   const CREATE_CITIES_TABLE = `
     CREATE TABLE IF NOT EXISTS cities (
-      city_number INT,
       city_name VARCHAR(50),
+      country VARCHAR(50),
       population FLOAT
     );`;
-
   connection.connect();
 
   try {
+    await execQuery(DROP_COUNTRIES_TABLE);
+    await execQuery(DROP_CITIES_TABLE);
     await execQuery(CREATE_DATABASE);
     await execQuery(CREATE_COUNTRIES_TABLE);
     await execQuery(CREATE_CITIES_TABLE);
-
-    data.countries.forEach(async country => {
-      await execQuery('INSERT INTO countries SET ?', country);
+    countries.forEach(async country => {
+      await execQuery('INSERT INTO countries SET name_en=?, continent=?, population=?, area=?', [
+        country.name_en,
+        country.continent,
+        country.population,
+        country.area,
+      ]);
     });
-    data.cities.forEach(async city => {
+    cities.forEach(async city => {
       await execQuery('INSERT INTO cities SET ?', city);
     });
   } catch (error) {
