1041e985b9bb8a20f2e564dcb25e679b5214f70f
unmeshvrije <unmesh.joshi126@gmail.com>
2019-06-10 11:56:09 +0200

    change database name in test

--- Week1/connection-test.js
+++ Week1/connection-test.js
@@ -3,7 +3,7 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'hyfuser',
   password : 'hyfpassword',
-  database : 'zoo'
+  database : 'userdb'
 });
 
 connection.connect();
