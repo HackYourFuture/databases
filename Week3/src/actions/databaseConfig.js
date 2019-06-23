d747a2fc4379e550f2f74cdcbe26aadd18fb8b82
Alaashhadat <loulousheha5@gmail.com>
2019-06-23 09:37:05 +0200

    Delete databaseConfig.js

--- Week3/src/actions/databaseConfig.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'hyfuser',
-  password: 'hyfpassword',
-  database: 'ToDoApp',
-});
-
-module.exports = connection;
