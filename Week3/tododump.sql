d3da9a16ef26b6c88396a25915369990662e37db
Akif <akbu2709@gmail.com>
2019-06-30 00:14:07 +0200

    Updates databasedump file

--- Week3/tododump.sql
+++ Week3/tododump.sql
@@ -7,7 +7,7 @@
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8mb4 ;
+SET NAMES utf8mb4 ;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -16,26 +16,40 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Current Database: `todo`
---
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `todo` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-
-USE `todo`;
-
---
 -- Table structure for table `category`
 --
 
 DROP TABLE IF EXISTS `category`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `category` (
-  `catID` int(20) NOT NULL AUTO_INCREMENT,
-  `categoryname` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`catID`)
+SET character_set_client
+= utf8mb4 ;
+CREATE TABLE `category`
+(
+  `catID` int
+(20) NOT NULL AUTO_INCREMENT,
+  `categoryname` varchar
+(50) DEFAULT NULL,
+  PRIMARY KEY
+(`catID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `
+category`
+VALUES
+  (1, 'scholl'),
+  (2, 'grocery'),
+  (3, 'Sport'),
+  (4, 'house'),
+  (5, 'work');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `iscomplete`
@@ -43,17 +57,46 @@ CREATE TABLE `category` (
 
 DROP TABLE IF EXISTS `iscomplete`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `iscomplete` (
-  `iscomplete` tinyint(1) DEFAULT '0',
-  `todoID` int(20) NOT NULL,
-  `listID` int(20) NOT NULL,
-  PRIMARY KEY (`todoID`,`listID`),
-  KEY `listID` (`listID`),
-  CONSTRAINT `iscomplete_ibfk_1` FOREIGN KEY (`todoID`) REFERENCES `todoitem` (`todoID`) ON DELETE CASCADE,
-  CONSTRAINT `iscomplete_ibfk_2` FOREIGN KEY (`listID`) REFERENCES `todolist` (`listID`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+SET character_set_client
+= utf8mb4 ;
+CREATE TABLE `iscomplete`
+(
+  `iscomplete` tinyint
+(1) DEFAULT '0',
+  `todoID` int
+(20) NOT NULL,
+  `listID` int
+(20) NOT NULL,
+  PRIMARY KEY
+(`todoID`,`listID`),
+  KEY `listID`
+(`listID`),
+  CONSTRAINT `iscomplete_ibfk_1` FOREIGN KEY
+(`todoID`) REFERENCES `todoitem`
+(`todoID`) ON
+DELETE CASCADE,
+  CONSTRAINT `iscomplete_ibfk_2` FOREIGN KEY
+(`listID`) REFERENCES `todolist`
+(`listID`) ON
+DELETE CASCADE
+) ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `iscomplete`
+--
+
+LOCK TABLES `iscomplete` WRITE;
+/*!40000 ALTER TABLE `iscomplete` DISABLE KEYS */;
+INSERT INTO `
+iscomplete`
+VALUES
+  (1, 1, 1),
+  (1, 3, 1),
+  (0, 10, 25);
+/*!40000 ALTER TABLE `iscomplete` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `todoitem`
@@ -61,13 +104,33 @@ CREATE TABLE `iscomplete` (
 
 DROP TABLE IF EXISTS `todoitem`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `todoitem` (
-  `todoID` int(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`todoID`)
+SET character_set_client
+= utf8mb4 ;
+CREATE TABLE `todoitem`
+(
+  `todoID` int
+(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar
+(50) DEFAULT NULL,
+  PRIMARY KEY
+(`todoID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `todoitem`
+--
+
+LOCK TABLES `todoitem` WRITE;
+/*!40000 ALTER TABLE `todoitem` DISABLE KEYS */;
+INSERT INTO `
+todoitem`
+VALUES
+  (1, 'Carrot'),
+  (3, 'strawberry'),
+  (10, 'Running Shorts');
+/*!40000 ALTER TABLE `todoitem` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `todolist`
@@ -75,20 +138,53 @@ CREATE TABLE `todoitem` (
 
 DROP TABLE IF EXISTS `todolist`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `todolist` (
-  `listID` int(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT NULL,
-  `userID` int(20) NOT NULL,
-  `catID` int(20) DEFAULT NULL,
+SET character_set_client
+= utf8mb4 ;
+CREATE TABLE `todolist`
+(
+  `listID` int
+(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar
+(50) DEFAULT NULL,
+  `userID` int
+(20) NOT NULL,
+  `catID` int
+(20) DEFAULT NULL,
   `deadline` datetime DEFAULT NULL,
-  PRIMARY KEY (`listID`),
-  KEY `userID` (`userID`),
-  KEY `catID` (`catID`),
-  CONSTRAINT `todolist_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`) ON DELETE CASCADE,
-  CONSTRAINT `todolist_ibfk_2` FOREIGN KEY (`catID`) REFERENCES `category` (`catID`) ON DELETE SET NULL
+  PRIMARY KEY
+(`listID`),
+  KEY `userID`
+(`userID`),
+  KEY `catID`
+(`catID`),
+  CONSTRAINT `todolist_ibfk_1` FOREIGN KEY
+(`userID`) REFERENCES `user`
+(`userID`) ON
+DELETE CASCADE,
+  CONSTRAINT `todolist_ibfk_2` FOREIGN KEY
+(`catID`) REFERENCES `category`
+(`catID`) ON
+DELETE
+SET NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `todolist`
+--
+
+LOCK TABLES `todolist` WRITE;
+/*!40000 ALTER TABLE `todolist` DISABLE KEYS */;
+INSERT INTO `
+todolist`
+VALUES
+  (1, 'mylist', 2, 1, '2019-06-30 00:00:00'),
+  (2, 'secondlist', 2, 1, '2019-06-01 00:00:00'),
+  (17, 'Thirdlist', 2, 4, '2012-06-20 00:00:00'),
+  (24, 'Homeworks', 2, 1, NULL),
+  (25, 'Spory Stuff', 2, 3, '2019-07-30 00:00:00');
+/*!40000 ALTER TABLE `todolist` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
@@ -96,14 +192,32 @@ CREATE TABLE `todolist` (
 
 DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `user` (
-  `userID` int(20) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(50) DEFAULT NULL,
-  `email` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`userID`)
+SET character_set_client
+= utf8mb4 ;
+CREATE TABLE `user`
+(
+  `userID` int
+(20) NOT NULL AUTO_INCREMENT,
+  `Name` varchar
+(50) DEFAULT NULL,
+  `email` varchar
+(50) DEFAULT NULL,
+  PRIMARY KEY
+(`userID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user`
+VALUES
+  (2, 'Hackyourfuture', 'hackyourfuture@gmail.com');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Dumping events for database 'todo'
@@ -122,15 +236,28 @@ CREATE TABLE `user` (
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `additem`(
-item_name VARCHAR(50),
-list_name VARCHAR(50)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `additem`
+(
+item_name VARCHAR
+(50),
+list_name VARCHAR
+(50)
 )
 BEGIN
-	DECLARE list_ID INT(20);
-SELECT listID INTO list_ID FROM todolist WHERE name = list_name;
-insert into todoitem (name) VALUES (item_name);
-insert into iscomplete (iscomplete, todoID, listID) VALUES (false, last_insert_id(), list_ID);
+  DECLARE list_ID INT
+  (20);
+SELECT listID
+INTO list_ID
+FROM todolist
+WHERE name = list_name;
+insert into todoitem
+  (name)
+VALUES
+  (item_name);
+insert into iscomplete
+  (iscomplete, todoID, listID)
+VALUES
+  (false, last_insert_id(), list_ID);
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -147,16 +274,27 @@ DELIMITER ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `addlist`(
-listname VARCHAR(50),
-username VARCHAR(50),
-catname VARCHAR(50),
+CREATE DEFINER=`root`@`localhost` PROCEDURE `addlist`
+(
+listname VARCHAR
+(50),
+username VARCHAR
+(50),
+catname VARCHAR
+(50),
 deadline_date DATETIME
 )
 BEGIN
-insert into todolist (name, userID, catID, deadline) VALUES (listname, 
-(SELECT userID from `user` WHERE name = username), 
-(SELECT catID from `category` WHERE categoryname = catname),
+  insert into todolist
+    (name, userID, catID, deadline)
+  VALUES
+    (listname,
+      (SELECT userID
+      from `user`
+      WHERE name = username),
+      (SELECT catID
+      from `category
+  ` WHERE categoryname = catname),
 deadline_date
 );
 
@@ -177,4 +315,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-06-27 17:11:14
+-- Dump completed on 2019-06-30  0:09:56
