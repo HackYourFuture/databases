80562b38b5e3478bc1a7c108b7dcaaf23c210372
aalhommada <aalhommada@gmail.com>
2019-07-30 10:41:35 +0200

    aalhommada-db-week1

--- Week1/node.js
+++ Week1/node.js
@@ -24,7 +24,7 @@ database.query(`USE world`, (error, result) => {
 //========== create tables : country , city , and insert data in it =================
 
 const table_of_country =
-  'CREATE TABLE IF NOT EXISTS country (name VARCHAR(50), continent VARCHAR(50), region VARCHAR(50), surface_area FLOAT, indep_year INT, population INT, life_expectancy, GNP FLOAT, GNPold FLOAT, local_name VARCHAR(50), government_form VARCHAR(50), head_of_state VARCHAR(50),capital VARCHAR(50))';
+  'CREATE TABLE IF NOT EXISTS country (name VARCHAR(50), continent VARCHAR(50), region VARCHAR(50), surface_area FLOAT, indep_year INT, population INT, life_expectancy FLOAT, GNP FLOAT, GNPold FLOAT, local_name VARCHAR(50), government_form VARCHAR(50), head_of_state VARCHAR(50),capital VARCHAR(50))';
 
 database.query(table_of_country, (error, result) => {
   if (error) throw error;
@@ -41,14 +41,15 @@ database.query(table_of_city, (error, results) => {
 });
 
 var insert_countries = [
-  "INSERT INTO country (name, continent,surface_area, population) VALUES ('Angola','Africa',1246700,12878000)",
-  "INSERT INTO country (name, continent,surface_area, population) VALUES ('Syria', 'Asia', 185180, 18269868)",
-  "INSERT INTO country (name, continent,surface_area, population) VALUES ('Tanzania', 'Africa', 883749, 57310019)",
-  "INSERT INTO country (name, continent,surface_area, population) VALUES ('Netherlands','Europe', 800, 17035938)",
-  "INSERT INTO country (name, continent,surface_area, population) VALUES ('Afghanistan', 'Asia',  652090, 35530081)",
-  "INSERT INTO country (name, continent,surface_area, population) VALUES ('Romania','Europe', 238391, 19679306)",
-  "INSERT INTO country (name, continent,surface_area, population) VALUES ('Switzerland','Europe', 41284, 8476005)",
-  "INSERT INTO country (name, continent,surface_area, population) VALUES ('Sweden', 'Europe', 449964, 9910701)",
+  'INSERT INTO country (name, continent, surface_area, population) VALUES' +
+    "                  ('Angola', 'Africa', 1246700, 12878000)," +
+    "                  ('Syria', 'Asia', 185180, 18269868),   " +
+    "                  ('Tanzania', 'Africa', 883749, 57310019)," +
+    "                  ('Netherlands','Europe', 800, 17035938)," +
+    "                  ('Afghanistan', 'Asia',  652090, 35530081)," +
+    "                  ('Romania','Europe', 238391, 19679306)," +
+    "                  ('Switzerland','Europe', 41284, 8476005)," +
+    "                  ('Sweden', 'Europe', 449964, 9910701),",
 ];
 for (let i in insert_countries) {
   database.query(insert_countries[i], error => {
@@ -60,20 +61,21 @@ for (let i in insert_countries) {
 }
 
 var insert_cities = [
-  "INSERT INTO city (id, name, country_code, population) VALUES (1, 'Amsterdam', 'NLD', 821752)",
-  "INSERT INTO city (id, name, country_code, population) VALUES (2, 'Rotterdam','NLD', 623652)",
-  "INSERT INTO city (id, name, country_code, population) VALUES (3, 'Kabul','KAB', 4600000)",
-  "INSERT INTO city (id, name, country_code, population) VALUES (4, 'Bucharest','BUC', 1836000)",
-  "INSERT INTO city (id, name, country_code, population) VALUES (5, 'Stockholm','STO',  965232)",
-  "INSERT INTO city (id, name, country_code, population) VALUES (6, 'Damascus', 'DAM',1711000)",
-  "INSERT INTO city (id, name, country_code, population) VALUES (7, 'Dodoma', 'DOD',2084000)",
-  "INSERT INTO city (id, name, country_code, population) VALUES (8, 'Bern', 'BER', 133115)",
-  "INSERT INTO city (id, name, country_code, population) VALUES (9,'Arnhem','NLD',138020)",
-  "INSERT INTO city (id, name, country_code, population) VALUES (10,'Breda','NLD',160398)",
-  "INSERT INTO city (id, name, country_code, population) VALUES (11,'Huambo','HUA',163100)",
-  "INSERT INTO city (id, name, country_code, population) VALUES (12,'Lobito','LOB',130000)",
-  "INSERT INTO city (id, name, country_code, population) VALUES (13,'Benguela','BEN',128300)",
-  "INSERT INTO city (id, name, country_code, population) VALUES (14,'Zaanstad', 'ZAA',135621)",
+  'INSERT INTO city (id, name, country_code, population) VALUES ' +
+    "(1, 'Amsterdam', 'NLD', 821752)" +
+    "(2, 'Rotterdam','NLD', 623652)" +
+    "(3, 'Kabul','KAB', 4600000)" +
+    "(4, 'Bucharest','BUC', 1836000)" +
+    "(5, 'Stockholm','STO',  965232)" +
+    "(6, 'Damascus', 'DAM',1711000)" +
+    "(7, 'Dodoma', 'DOD',2084000)" +
+    "(8, 'Bern', 'BER', 133115)" +
+    "(9,'Arnhem','NLD',138020)" +
+    "(10,'Breda','NLD',160398)" +
+    "(11,'Huambo','HUA',163100)" +
+    "(12,'Lobito','LOB',130000)" +
+    "(13,'Benguela','BEN',128300)" +
+    "(14,'Zaanstad', 'ZAA',135621)",
 ];
 
 for (let i in insert_cities) {
@@ -97,7 +99,7 @@ database.query(`SELECT * FROM country WHERE Population > 8000000`, (error, resul
 });
 
 // Names of the countries that have “land” in their names ?
-database.query(`SELECT * FROM country WHERE Name LIKE '%land%' `, (error, result) => {
+database.query(`SELECT name FROM country WHERE Name LIKE '%land%' `, (error, result) => {
   if (error) throw error;
   console.log(`\n Countries that have “land” in their names:`);
   result.forEach(land => {
@@ -130,7 +132,7 @@ database.query(`SELECT * FROM country WHERE Continent = 'Europe' `, (error, resu
 
 // Countries in descending order, based on their surface areas.
 
-database.query(`SELECT * FROM country ORDER BY SurfaceArea DESC`, (error, result) => {
+database.query(`SELECT * FROM country ORDER BY surface_area DESC`, (error, result) => {
   if (error) throw error;
 
   console.log(`\n Countries in descending order based on their surface areas:`);
@@ -172,7 +174,7 @@ database.query('SELECT * FROM country ORDER BY surface_area DESC LIMIT 10', (err
 });
 
 // top 10 cities with the highest population?
-database.query('SELECT FROM city ORDER BY population DESC LIMIT 10', (error, result) => {
+database.query('SELECT * FROM city ORDER BY population DESC LIMIT 10', (error, result) => {
   if (error) throw error;
 
   console.log(`\n Top 10 cities with the highest population:`);
