d021c55f55e4cb47cab7967ff15eb2714743c4fa
zehra kopar <48477070+zehrakopar@users.noreply.github.com>
2019-08-01 12:54:04 +0200

    update node.js
    changes done.

--- Week1/node.js
+++ Week1/node.js
@@ -48,7 +48,7 @@ const selectQueries = [
   'SELECT Name, Population FROM country WHERE Population > 8000000',
   "SELECT Name FROM country WHERE Name LIKE '%land%'",
   'SELECT Name, Population FROM city WHERE Population BETWEEN 500000 AND 1000000',
-  "SELECT Name, Continent FROM country WHERE Continent = 'Europe'",
+  "SELECT Name FROM country WHERE Continent = 'Europe'",
   'SELECT Name,  SurfaceArea FROM country ORDER BY SurfaceArea DESC',
   "SELECT Name, CountryCode FROM city WHERE CountryCode = 'NLD'",
   "SELECT Name, Population FROM city WHERE Name = 'Rotterdam'",
