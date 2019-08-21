01ac1920c2816b52f0c46d3612e667f4f37f08e3
zehrakopar <zehrakopar@yahoo.com.tr>
2019-08-21 14:52:21 +0200

    week3

--- Week3/app.js
+++ Week3/app.js
@@ -16,30 +16,34 @@ const connection = mysql.createConnection({
 
 const execQuery = util.promisify(connection.query.bind(connection));
 
-connection.connect();
 
-// get todo list
-app.get('/todolist/:id', async (req, res) => {
+
+// get todo lists
+app.get('/todolist', async (req, res) => {
   try {
-    const query1 = 'SELECT * from todolist WHERE ID=?';
-    await execQuery(query1, req.params.id);
+    
+    const query1 = 'SELECT * from todolist';
+    await execQuery(query1);
+    
     res.status(200).json({
-      message: 'the todo list with the id of ${req.params.id}',
+      message: 'the todo lists',
     });
+    
   } catch (error) {
     res.status(500).json({
       error: error.message,
     });
+   
   }
 });
 
-// get todo items
-app.get('/todoitems/:listId', async (req, res) => {
+// get todo items of a todo list
+app.get('/todoitems', async (req, res) => {
   try {
     const query2 = 'SELECT * from todoitem WHERE TodoListID=?';
-    await execQuery(query2, req.params.listId);
+    await execQuery(query2, req.body.listId);
     res.status(200).json({
-      message: 'the todo items which are in the list of ${req.params.listId}',
+      message: 'the todo items which are in the list of ${req.body.listId}',
     });
   } catch (error) {
     res.status(500).json({
@@ -49,13 +53,13 @@ app.get('/todoitems/:listId', async (req, res) => {
 });
 
 // Insert item(s) in ToDo list
-app.post('/insertTodoItems/:listId', async (req, res) => {
+app.post('/todoitem', async (req, res) => {
   try {
     const query3 =
       'INSERT INTO todoitem (ID, description, ,TodoListID, completed) values(NULL,?,?,?)';
-    await execQuery(query3, [req.body.description, req.params.listId, req.body.completed]);
+    await execQuery(query3, [req.body.description, req.body.listId, req.body.completed]);
     res.status(200).json({
-      message: 'inserted a todo item',
+      message: 'created a new todo item',
     });
   } catch (error) {
     res.status(500).json({
@@ -65,10 +69,10 @@ app.post('/insertTodoItems/:listId', async (req, res) => {
 });
 
 // Delete item(s) in ToDo list
-app.delete('/:listId/deleteTodoItems/:id', async (req, res) => {
+app.delete('/todoitem/:id', async (req, res) => {
   try {
     const query4 = 'DELETE FROM todoitem WHERE ID =? AND TodoListID =?';
-    await execQuery(query4, [req.params.id, req.params.listId]);
+    await execQuery(query4, [req.params.id, req.body.listId]);
     res.status(200).json({
       message: `todo with the id of ${req.params.id} is removed`,
     });
@@ -80,12 +84,12 @@ app.delete('/:listId/deleteTodoItems/:id', async (req, res) => {
 });
 
 // Create a new ToDo list
-app.post('/createList', async (req, res) => {
+app.post('/todolist', async (req, res) => {
   try {
     const query5 = 'INSERT INTO todolist (ID, Name, UserID, Reminder) values(NULL,?,?,NULL)';
     await execQuery(query5, [req.body.Name, req.body.UserID]);
     res.status(200).json({
-      message: 'a new list created',
+      message: 'a new list is created',
     });
   } catch (error) {
     res.status(500).json({
@@ -95,12 +99,12 @@ app.post('/createList', async (req, res) => {
 });
 
 // Delete a ToDo list
-app.delete('/deleteTodoList/:id', async (req, res) => {
+app.delete('/todolist/:listId', async (req, res) => {
   try {
     const query6 = 'DELETE FROM todolist WHERE ID=?';
-    await execQuery(query6, req.params.id);
+    await execQuery(query6, req.params.listId);
     res.status(200).json({
-      message: 'todo list with the id of ${req.params.id} is deleted',
+      message: 'todo list with the id of ${req.params.listId} is deleted',
     });
   } catch (error) {
     res.status(500).json({
@@ -110,10 +114,10 @@ app.delete('/deleteTodoList/:id', async (req, res) => {
 });
 
 // Mark an item as completed
-app.put('/:listId/markAsCompleted/:id', async (req, res) => {
+app.put('/todoitem/:id', async (req, res) => {
   try {
     const query7 = 'UPDATE todoitem SET completed= 1 WHERE ID=? AND TodoListID=?';
-    await execQuery(query7, [req.params.id, req.params.listId]);
+    await execQuery(query7, [req.params.id, req.body.listId]);
     res.status(200).json({
       message: 'todo item with the id of ${req.params.id} is completed',
     });
@@ -125,12 +129,12 @@ app.put('/:listId/markAsCompleted/:id', async (req, res) => {
 });
 
 // Add a reminder for the list (not for the item)
-app.post('/:Id/reminder', async (req, res) => {
+app.post('/todolist/:listId/reminder', async (req, res) => {
   try {
     const query8 = 'UPDATE todolist SET Reminder=? WHERE ID=?';
-    await execQuery(query8, [req.body.Reminder, req.params.Id]);
+    await execQuery(query8, [req.body.Reminder, req.params.listId]);
     res.status(200).json({
-      message: 'reminder date added to the TodoList with the id of ${req.params.Id}',
+      message: 'reminder date added to the TodoList with the id of ${req.params.listId}',
     });
   } catch (error) {
     res.status(500).json({
@@ -139,7 +143,7 @@ app.post('/:Id/reminder', async (req, res) => {
   }
 });
 
-connection.end();
+
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
