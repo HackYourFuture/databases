1b569ccd3c0c2a367bb1191f79d9607dca821a4d
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-09-11 18:09:24 +0200

    few changed to  VARCHAR

--- Week1/homework/queries_db_tb.js
+++ Week1/homework/queries_db_tb.js
@@ -6,27 +6,27 @@ const creatDbWorld = `CREATE SCHEMA world`;
 const dropTableCountry = `DROP TABLE IF EXISTS country`;
 const CreateTableCountry = `CREATE TABLE  country  (
   Code  CHAR(3) NOT NULL DEFAULT '',
-  Name  CHAR(52) NOT NULL DEFAULT '',
+  Name  VARCHAR(52) NOT NULL DEFAULT '',
   Continent  enum('Asia','Europe','North America','Africa','Oceania','Antarctica','South America') NOT NULL DEFAULT 'Asia',
-  Region  CHAR(26) NOT NULL DEFAULT '',
+  Region  VARCHAR(26) NOT NULL DEFAULT '',
   SurfaceArea  FLOAT(10,2) NOT NULL DEFAULT '0.00',
   IndepYear  SMALLINT(6) ,
   Population  INT(11) NOT NULL DEFAULT '0',
   LifeExpectancy  FLOAT(3,1) ,
   GNP  FLOAT(10,2) ,
   GNPOld  FLOAT(10,2) ,
-  LocalName  CHAR(45) NOT NULL DEFAULT '',
-  GovernmentForm  CHAR(45) NOT NULL DEFAULT '',
-  HeadOfState  CHAR(60) ,
-  Capital  CHAR(22) NOT NULL DEFAULT ''
+  LocalName  VARCHAR(45) NOT NULL DEFAULT '',
+  GovernmentForm  VARCHAR(45) NOT NULL DEFAULT '',
+  HeadOfState  VARCHAR(60) ,
+  Capital  VARCHAR(22) NOT NULL DEFAULT ''
 ) `;
 
 const dropTableCity = `DROP table IF EXISTS city`;
 const CreateTableCity = `CREATE TABLE city (
   ID INT(11) NOT NULL AUTO_INCREMENT,
-  Name CHAR(35) NOT NULL DEFAULT '',
+  Name VARCHAR(35) NOT NULL DEFAULT '',
   CountryCode CHAR(3) NOT NULL DEFAULT '',
-  District CHAR(20) NOT NULL DEFAULT '',
+  District VARCHAR(20) NOT NULL DEFAULT '',
   Population INT(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (ID)
 )`;
