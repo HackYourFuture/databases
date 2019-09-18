08dbbf43296385d5bf872d66de23d7b20726725a
Gul <gulpembeyener@gmail.com>
2019-09-18 15:22:46 +0200

    Change varchar to enum

--- Week1/homework/create-db-tables.js
+++ Week1/homework/create-db-tables.js
@@ -14,7 +14,7 @@ const sqlCommand = `
 
   USE new_world; 
 
-  CREATE TABLE if not exists country(id INT AUTO_INCREMENT PRIMARY KEY, Name VARCHAR(50), Continent VARCHAR(50), Region VARCHAR(50), SurfaceArea FLOAT, IndepYear INT, Population INT, LifeExpectancy FLOAT, GNP FLOAT(7,2), GNPOld FLOAT(7,2), LocalName VARCHAR(50), GovernmentForm VARCHAR(50), HeadOfState VARCHAR(50), Capital VARCHAR(30));
+  CREATE TABLE if not exists country(id INT AUTO_INCREMENT PRIMARY KEY, Name VARCHAR(50), Continent ENUM('Asia', 'Africa', 'North America', 'South America', 'Antarctica','Europe', 'Australia', 'Oceania'), Region VARCHAR(50), SurfaceArea FLOAT, IndepYear INT, Population INT, LifeExpectancy FLOAT, GNP FLOAT(7,2), GNPOld FLOAT(7,2), LocalName VARCHAR(50), GovernmentForm VARCHAR(50), HeadOfState VARCHAR(50), Capital VARCHAR(30));
 
   CREATE TABLE if not exists city(id INT AUTO_INCREMENT PRIMARY KEY, Name VARCHAR(50), CountryCode VARCHAR(10), District VARCHAR(30), Population INT);
 `;
