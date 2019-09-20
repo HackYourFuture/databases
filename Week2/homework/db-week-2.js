502bd5e049c6812bc879b3c4a63497883bb1892d
elicarcar <elisabetta.carcar@hotmail.com>
2019-09-20 13:46:42 +0200

    Finished most of the work

--- Week2/homework/db-week-2.js
+++ Week2/homework/db-week-2.js
@@ -17,52 +17,78 @@ connection_config.connect(err => {
   }
 });
 
-program.command('get-capital-of <country>').action(country => {
-  const getCapital = `select city.name as Capital from country, city where country.capital = city.id and country.name ='${country}';`;
-  execQuery(getCapital).then(capital => {
-    console.log(`${JSON.stringify(capital)}`);
+program.version('0.0.1');
+
+program.description(
+  'Hello! Welcome to this small cli program.\nHere we provide you some information based on what you specify.\nLook below and start exploring!',
+);
+
+program
+  .command('capital-of <country>')
+  .description('This command gives you the capital of the country')
+  .action(country => {
+    const getCapital = `select city.name as Capital from country, city where country.capital = city.id and country.name ='${country}';`;
+    try {
+      execQuery(getCapital).then(capital => {
+        console.log(`${JSON.stringify(capital)}`);
+      });
+    } catch (err) {
+      if (err) {
+        throw err;
+      }
+    }
   });
-});
 
-program.command('get-spoken-language <region>').action(region => {
-  const getRegion = `select distinct language from countrylanguage, country where countrylanguage.CountryCode = country.code and country.region ='${region}';`;
-  try {
-    execQuery(getRegion).then(region => {
-      console.log(`${JSON.stringify(region)}`);
-    });
-  } catch (err) {
-    if (err) {
-      throw err;
+program
+  .command('spoken-language <region>')
+  .description("This command gives you the spoken language on the region you're looking at")
+  .action(region => {
+    const getRegion = `select distinct language from countrylanguage, country where countrylanguage.CountryCode = country.code and country.region ='${region}';`;
+    try {
+      execQuery(getRegion).then(region => {
+        console.log(`${JSON.stringify(region)}`);
+      });
+    } catch (err) {
+      if (err) {
+        throw err;
+      }
     }
-  }
-});
+  });
 
-program.command('get-city-number <language>').action(language => {
-  const getCityNumber = `select distinct count(name) as 'Number of Cities' from city, countrylanguage where countrylanguage.countrycode = city.countrycode and countrylanguage.language='${language}';`;
-  try {
-    execQuery(getCityNumber).then(number => {
-      console.log(`${JSON.stringify(number)}`);
-    });
-  } catch (err) {
-    if (err) {
-      throw err;
+program
+  .command('get-city-number <language>')
+  .description(
+    'This command gives you the number of cities that the language you specify is spoken.',
+  )
+  .action(language => {
+    const getCityNumber = `select distinct count(name) as 'Number of Cities' from city, countrylanguage where countrylanguage.countrycode = city.countrycode and countrylanguage.language='${language}';`;
+    try {
+      execQuery(getCityNumber).then(number => {
+        console.log(`${JSON.stringify(number)}`);
+      });
+    } catch (err) {
+      if (err) {
+        throw err;
+      }
     }
-  }
-});
+  });
 
-program.command('languages-by-continents').action(langContTable => {
-  const getLangNumByCont = `select Continent, count(distinct Language) as Languages from country, countrylanguage where country.Code=countrylanguage.CountryCode group by Continent;`;
-  try {
-    execQuery(getLangNumByCont).then(list => {
-      const listt = JSON.stringify(list);
-      console.log(JSON.parse(listt));
-    });
-  } catch (err) {
-    if (err) {
-      console.log(err);
+program
+  .command('languages-by-continents')
+  .description('Gives you the number of spoken languages on each continent ')
+  .action(() => {
+    const getLangNumByCont = `select Continent, count(distinct Language) as Languages from country, countrylanguage where country.Code=countrylanguage.CountryCode group by Continent;`;
+    try {
+      execQuery(getLangNumByCont).then(list => {
+        const listt = JSON.stringify(list);
+        console.log(JSON.parse(listt));
+      });
+    } catch (err) {
+      if (err) {
+        console.log(err);
+      }
     }
-  }
-});
+  });
 
 program.parse(process.argv);
 
