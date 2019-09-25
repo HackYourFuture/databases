422d04e62d9309ff8a7e1c22dff6f992405b5148
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-09-25 21:54:26 +0200

    validate req.body lists

--- Week3/homework/api/routes/lists_routes.js
+++ Week3/homework/api/routes/lists_routes.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const Joi = require('@hapi/joi');
 
 router.post('/', (req, res, next) => {
+    const schema = Joi.object({
+        listName: Joi.string()
+            .min(3)
+            .max(42)
+            .required(),
+        userIdReference: Joi.number()
+            .min(1)
+            .required(),
+    });
+    const validationResult = schema.validate({
+        listName: req.body.listName,
+        userIdReference: req.body.userIdReference,
+    });
+    if (validationResult.error) {
+        const message = validationResult.error.details[0].message;
+        res.status(400).send(`<p>${message}</p>`);
+        return;
+    }
+
     const listName = req.body.listName;
     const userIdReference = req.body.userIdReference;
     res.status(201).json({
@@ -19,6 +39,20 @@ router.delete('/:listId', (req, res, next) => {
 
 router.patch('/:listId', (req, res, next) => {
     const listId = req.params.listId;
+    const schema = Joi.object({
+        listReminder: Joi.number()
+            .min(1)
+            .max(31)
+            .required(),
+    });
+    const validationResult = schema.validate({
+        listReminder: req.body.listReminder,
+    });
+    if (validationResult.error) {
+        const message = validationResult.error.details[0].message;
+        res.status(400).send(`<p>${message}</p>`);
+        return;
+    }
     const listReminder = req.body.listReminder;
     res.status(200).json({
         message: `Updated list with ID ${listId}`,
