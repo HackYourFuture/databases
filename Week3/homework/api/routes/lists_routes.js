9ef8adf7612c4581cdaee694557e1f5672d7162d
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-09-25 16:54:46 +0200

    add const userIdReference

--- Week3/homework/api/routes/lists_routes.js
+++ Week3/homework/api/routes/lists_routes.js
@@ -3,9 +3,17 @@ const router = express.Router();
 
 router.post('/', (req, res, next) => {
     const listName = req.body.listName;
+    const userIdReference = req.body.userIdReference;
     res.status(201).json({
         message: 'Handling POST requests to list',
-        info: ` the list name is: ${listName}`,
+        info: ` the list name is: ${listName} for the user with ID: ${userIdReference}`,
+    });
+});
+
+router.delete('/:listId', (req, res, next) => {
+    const listId = req.params.listId;
+    res.status(200).json({
+        message: `Deleted list with ID ${listId}`,
     });
 });
 
@@ -15,13 +23,6 @@ router.patch('/:listId', (req, res, next) => {
     res.status(200).json({
         message: `Updated list with ID ${listId}`,
         info: ` the list has a reminder and it is: ${listReminder}`,
-    });
-});
-
-router.delete('/:listId', (req, res, next) => {
-    const listId = req.params.listId;
-    res.status(200).json({
-        message: `Deleted list with ID ${listId}`,
     });
 });
 
