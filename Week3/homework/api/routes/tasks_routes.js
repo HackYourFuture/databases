bec444dd44330e9ead0748800db5c6126ab71429
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-09-25 16:56:03 +0200

    get task/tasks from body req

--- Week3/homework/api/routes/tasks_routes.js
+++ Week3/homework/api/routes/tasks_routes.js
@@ -2,8 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', (req, res, next) => {
+    const tasks = req.body.tasks;
     res.status(201).json({
         message: 'Handling POST requests to task',
+        info: ` the task description is: ${JSON.stringify(tasks)}`,
+    });
+});
+
+router.delete('/', (req, res, next) => {
+    const tasksIds = req.body.tasskIds;
+    res.status(200).json({
+        message: `Deleted task with id ${tasksId}`,
     });
 });
 
@@ -11,13 +20,7 @@ router.patch('/:taskId', (req, res, next) => {
     const taskId = req.params.taskId;
     res.status(200).json({
         message: `Update task with id ${taskId}`,
-    });
-});
-
-router.delete('/:taskId', (req, res, next) => {
-    const taskId = req.params.taskId;
-    res.status(200).json({
-        message: `Deleted task with id ${taskId}`,
+        info: ` the task is marked as completed`,
     });
 });
 
