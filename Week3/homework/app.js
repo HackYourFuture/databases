6ea7c666e8a3f5a3d00abcf4ef864d2ca91843ac
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-09-25 15:40:44 +0200

    dealing with undefined endpoints.

--- Week3/homework/app.js
+++ Week3/homework/app.js
@@ -11,4 +11,19 @@ app.all('/', (req, res, next) => {
 app.use('/lists', lists_routes);
 app.use('/tasks', tasks_routes);
 
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message,
+        },
+    });
+});
+
 module.exports = app;
