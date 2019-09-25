2cb92867616588b2ef3d7d91a148441fba1b92dc
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-09-25 16:33:44 +0200

    get list/lists form body req

--- Week3/homework/api/routes/lists_routes.js
+++ Week3/homework/api/routes/lists_routes.js
@@ -2,15 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', (req, res, next) => {
+    const listName = req.body.listName;
     res.status(201).json({
         message: 'Handling POST requests to list',
+        info: ` the list name is: ${listName}`,
     });
 });
 
 router.patch('/:listId', (req, res, next) => {
     const listId = req.params.listId;
+    const listReminder = req.body.listReminder;
     res.status(200).json({
         message: `Updated list with ID ${listId}`,
+        info: ` the list has a reminder and it is: ${listReminder}`,
     });
 });
 
