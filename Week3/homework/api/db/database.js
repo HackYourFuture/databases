a3c3418e00f58d404401f997968ff9c8fe8a3b2d
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-09-26 06:16:02 +0200

    add file with mysql connection

---
+++ Week3/homework/api/db/database.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'hyfuser',
+    password: 'hyfpassword',
+    database: '',
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('connected as id ' + connection.threadId);
+});
+module.exports = { connection };
