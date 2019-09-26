e8c13265f6f09619aba6a126e85ed86ba7de1ac8
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-09-26 06:32:29 +0200

    var => const

--- Week3/homework/api/db/database.js
+++ Week3/homework/api/db/database.js
@@ -1,5 +1,5 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
+const mysql = require('mysql');
+const connection = mysql.createConnection({
     host: 'localhost',
     user: 'hyfuser',
     password: 'hyfpassword',
