d6a8d66144f0ef8a725afdf21b7572b3966b8867
Laurelinduilwen <ladylaurelin@hotmail.com>
2019-10-08 00:31:41 +0200

    implemented feedback for databases week02

--- Week2/week02.js
+++ Week2/week02.js
@@ -12,12 +12,25 @@ const execQuery = util.promisify(connection.query.bind(connection));
 
 connection.connect();
 
-const queries = [
-  'SELECT name FROM city WHERE id IN(SELECT capital FROM country WHERE name = ?);',
-  'SELECT distinct language FROM countrylanguage WHERE countrycode IN (SELECT code FROM country WHERE region = ?);',
-  'SELECT COUNT(name) AS "total cities" FROM city WHERE countrycode IN(SELECT countrycode FROM countrylanguage WHERE language = ?);',
-  'SELECT name FROM country WHERE region = ? AND code IN(SELECT countrycode FROM countrylanguage WHERE language = ? AND isofficial = "T");',
-  'SELECT name FROM country WHERE region = ? AND code IN(SELECT countrycode FROM countrylanguage WHERE language = ? AND isofficial = "T");',
-];
+async function submitQueries() {
+  const queries = [
+    'SELECT name FROM city WHERE id IN(SELECT capital FROM country WHERE name = ?);',
+    'SELECT distinct language FROM countrylanguage WHERE countrycode IN (SELECT code FROM country WHERE region = ?);',
+    'SELECT COUNT(name) AS "total cities" FROM city WHERE countrycode IN(SELECT countrycode FROM countrylanguage WHERE language = ?);',
+    'SELECT name FROM country WHERE region = ? AND code IN(SELECT countrycode FROM countrylanguage WHERE language = ? AND isofficial = "T");',
+    'SELECT country.continent, COUNT(countrylanguage.language) AS FROM country JOIN countrylanguage ON country.code = countrylanguage.countrycode GROUP BY country.continent);',
+  ];
 
-connection.end();
+  connection.connect();
+
+  try {
+    queries.forEach(async queries => {
+      await execQuery('running', queries);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+
+  connection.end();
+}
+submitQueries();
