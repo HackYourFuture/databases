826d245fbe06cff63475ba15582728279770edbe
Laurelinduilwen <ladylaurelin@hotmail.com>
2019-10-07 23:56:42 +0200

    Implemented feedback for databases week01

--- Week1/Week01.js
+++ Week1/Week01.js
@@ -10,7 +10,7 @@ const database = 'create database if not exists world';
 const useDatabase = 'use world';
 
 const countries =
-  'create table country (name varchar(50), continent varchar(50), region varchar(50), surface_area int, independence_year date, population int, gnp int, gnp_old int, local_name varchar(50), government_form varchar(50), head_of_state text, capital varchar(50))';
+  'create table country (name varchar(50), continent varchar(50), region varchar(50), surface_area float, independence_year date, population int, gnp float, gnp_old float, local_name varchar(50), government_form varchar(50), head_of_state text, capital varchar(50))';
 const cities =
   'create table city (id int, name varchar(50), country_code varchar(3), district varchar(50), population int)';
 var create_queries = [database, useDatabase, countries, cities];
@@ -18,6 +18,27 @@ var create_queries = [database, useDatabase, countries, cities];
 for (var i in create_queries) {
   console.log('Going to run ', create_queries[i]);
   connection.query(create_queries[i], function(error, fields) {
+    if (error) {
+      throw error;
+    }
+  });
+}
+
+//manually entering these took more time than the homework itself >_>, values might not represent real world data...
+
+const inserts = [
+  `insert into country values('United States of America', 'North America', 'Anglo-America', 9147590, '1776-07-04:00:00:00', 327167434, 20494000000000, 20294000000000, 'United States of America', 'Federal presidential constitutional republic', 'Donald J. Trump', 'Washington D.C.')`,
+  `insert into country values('Netherlands', 'Europe', 'Low Countries', 41543, '1581-07-26:00:00:00', 17336891, 1044000000000, 1034000000000, 'Nederland', 'Unitary parliamentary constitutional monarchy', 'Willem-Alexander', 'Amsterdam')`,
+  `insert into country values('Turkey', 'Asia', 'Middle East', 783356, '1923-10-29:00:00:00', 82003882, 706237000000., 20294000000000, 'Turkiye Cumhuriyeti', 'Unitary presidential constitutional republic', 'Some watermellon seller', 'Ankara')`,
+  `insert into city values(01, 'Ankara', 'TR', '', 20000000)`,
+  `insert into city values(02, 'Washington D.C.', 'USA', '', 4354345)`,
+  `insert into city values(03, 'Amsterdam', 'NL', 'North Holland', 741636)`,
+  `insert into city values(04, 'Rotterdam', 'NL', 'South Holland', 598199)`,
+];
+
+for (let i in inserts) {
+  console.log('Running ', inserts[i]);
+  connection.query(inserts[i], function(error, results, fields) {
     if (error) {
       throw error;
     }
