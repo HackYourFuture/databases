31ce914f6d817b51fcbb898770b48c33ef8674f8
msnmz <snmez.muharrem@gmail.com>
2019-10-27 21:52:09 +0100

    connects succesfully to the mysql server

--- Week1/index.js
+++ Week1/index.js
@@ -1,1 +1,17 @@
 const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+});
+
+connection.connect(err => {
+  if (err) {
+    console.log(`Connection Error: ${err.message} (${err.code})`);
+    process.exit();
+  }
+  console.log('Successfully connected to mysql server...');
+});
+
+connection.end();
