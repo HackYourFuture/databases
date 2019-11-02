140154480a6c107e565914e7093db9cd4d9208f6
mehmetekinci <mehmetekinci1989@gmail.com>
2019-11-02 02:07:31 +0100

    improved some codes

--- Week1/index.js
+++ Week1/index.js
@@ -6,6 +6,73 @@ const readFilePromisify = promisify(fs.readFile);
 const filePath = './data.json';
 const defaultEncoding = 'utf-8';
 
+const createHelpOptions = () => {
+  console.log(`Usage: node index.js [options]
+
+Options:
+
+1          shows answer of "What are the names of the countries 
+                             with population greater than 8 million?"
+2          shows answer of "What are the names of the countries 
+                             that have “land” in their names ?"
+3          shows answer of "What are the names of the cities with
+                             population in between 500,000 and 1 million ?"
+4          shows answer of "What are the names of all the countries
+                             on the continent ‘Europe’ ?"
+5          shows answer of List all the countries in the descending order
+                             based on their surface areas.        
+6          shows answer of "What are the names of all the cities in the
+                             Netherlands?"
+7          shows answer of "What's the population of Rotterdam?"
+8          shows answer of "What's the top 10 countries based on surface area?"
+9          shows answer of "What's the top 10 cities with the highest population?"
+10         shows answer of "What's the population of the world ?"
+  `);
+};
+
+const queries = [
+  {
+    query: `select name,population from countries where population > 8000000;`,
+    question: 1,
+  },
+  {
+    query: `select name from countries where name like '%land%';`,
+    question: 2,
+  },
+  {
+    query: `select name,population from cities where population >500000 and population < 1000000;`,
+    question: 3,
+  },
+  {
+    query: `select name from countries where continent = 'Europe';`,
+    question: 4,
+  },
+  {
+    query: `select * from countries order by surface_area desc;`,
+    question: 5,
+  },
+  {
+    query: `select name,population from cities where country_code='NLD';`,
+    question: 6,
+  },
+  {
+    query: `select name,population from cities where name='Rotterdam';`,
+    question: 7,
+  },
+  {
+    query: `select name,surface_area from countries order by surface_area desc limit 10;`,
+    question: 8,
+  },
+  {
+    query: `select name,population from cities order by population desc limit 10;`,
+    question: 9,
+  },
+  {
+    query: `select sum(population) as world_population from countries;`,
+    question: 10,
+  },
+];
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -14,18 +81,27 @@ const connection = mysql.createConnection({
 
 const execQuery = promisify(connection.query.bind(connection));
 
+const consoleQuery = async question => {
+  queries.filter(query => {
+    if (query.question === question) {
+      console.log(`ANSWER OF QUESTION ${query.question} :`);
+      return execQuery(query.query).then(console.table);
+    }
+  });
+};
+
 async function seedDatabase() {
   const DELETE_DATABASE = `DROP DATABASE IF EXISTS WORLD;`;
   const CREATE_DATABASE_WORLD = `CREATE DATABASE IF NOT EXISTS world;`;
   const USE_DATABASE_WORLD = `use world;`;
   const CREATE_CITIES_TABLE = `
-    CREATE TABLE IF NOT EXISTS cities (
-      id INT NOT NULL AUTO_INCREMENT,
-      name VARCHAR(254),
-      country_code VARCHAR(3),
-      district VARCHAR(254),
-      population INT,
-      PRIMARY KEY (id)
+  CREATE TABLE IF NOT EXISTS cities (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(254),
+    country_code VARCHAR(3),
+    district VARCHAR(254),
+    population INT,
+    PRIMARY KEY (id)
     );`;
   const CREATE_COUNTRIES_TABLE = `
     CREATE TABLE IF NOT EXISTS countries (
@@ -44,7 +120,7 @@ async function seedDatabase() {
       head_of_state VARCHAR(254),
       capital INT,
       PRIMARY KEY (id)
-    );`;
+      );`;
 
   connection.connect();
 
@@ -62,7 +138,7 @@ async function seedDatabase() {
       try {
         await execQuery(
           `INSERT INTO cities SET name= '${city.name}', country_code= '${city.country_code}',
-        district= '${city.district}', population= ${city.population}`,
+            district= '${city.district}', population= ${city.population}`,
           city,
         );
       } catch (error) {
@@ -74,73 +150,58 @@ async function seedDatabase() {
       try {
         await execQuery(
           `INSERT INTO countries SET name= "${country.name}", continent= "${country.continent}",
-        region= "${country.region}", surface_area= ${country.surface_area},
-        indep_year= ${country.indep_year}, population= ${country.population},
-        life_expectancy= ${country.life_expectancy}, gnp= ${country.gnp},
-        gnp_old= ${country.gnp_old}, local_name= "${country.local_name}",
-        government_form= "${country.government_form}" , head_of_state= "${country.head_of_state}",
-        capital= ${country.capital}`,
+              region= "${country.region}", surface_area= ${country.surface_area},
+              indep_year= ${country.indep_year}, population= ${country.population},
+              life_expectancy= ${country.life_expectancy}, gnp= ${country.gnp},
+              gnp_old= ${country.gnp_old}, local_name= "${country.local_name}",
+              government_form= "${country.government_form}" , head_of_state= "${country.head_of_state}",
+              capital= ${country.capital}`,
           country,
         );
       } catch (error) {
         console.log(error);
       }
     });
-
-    // answers of question 4
-    const answer4_1 = await execQuery(
-      `select name,population from countries where population > 8000000;`,
-    );
-    console.log('ANSWER OF QUESTION 4.1 :');
-    console.table(answer4_1);
-    const answer4_2 = await execQuery(
-      `select name from countries where name like '%land%';`,
-    );
-    console.log('ANSWER OF QUESTION 4.2 :');
-    console.table(answer4_2);
-    const answer4_3 = await execQuery(
-      `select name,population from cities where population >500000 and population < 1000000;`,
-    );
-    console.log('ANSWER OF QUESTION 4.3 :');
-    console.table(answer4_3);
-    const answer4_4 = await execQuery(
-      `select name from countries where continent = 'Europe';`,
-    );
-    console.log('ANSWER OF QUESTION 4.4 :');
-    console.table(answer4_4);
-    const answer4_5 = await execQuery(`select * from countries order by surface_area desc;
-    `);
-    console.log('ANSWER OF QUESTION 4.5 :');
-    console.table(answer4_5);
-
-    // answers of question 5
-    const answer5_1 = await execQuery(
-      `select name,population from cities where country_code='NLD';`,
-    );
-    console.log('ANSWER OF QUESTION 5.1 :');
-    console.table(answer5_1);
-    const answer5_2 = await execQuery(
-      `select name,population from cities where name='Rotterdam';`,
-    );
-    console.log('ANSWER OF QUESTION 5.2 :');
-    console.table(answer5_2);
-    const answer5_3 = await execQuery(
-      `select name,surface_area from countries order by surface_area desc limit 10;`,
-    );
-    console.log('ANSWER OF QUESTION 5.3 :');
-    console.table(answer5_3);
-    const answer5_4 = await execQuery(
-      `select name,population from cities order by population desc limit 10;`,
-    );
-    console.log('ANSWER OF QUESTION 5.4:');
-    console.table(answer5_4);
-    const answer5_5 = await execQuery(
-      `select sum(population) as world_population from countries;`,
-    );
-    console.log('ANSWER OF QUESTION 5.5 :');
-    console.table(answer5_5);
   } catch (error) {
     console.error(error);
+  }
+
+  const [, , ...questions] = process.argv;
+
+  switch (questions[0]) {
+    case '1':
+      consoleQuery(parseInt(questions[0], 10));
+      break;
+    case '2':
+      consoleQuery(parseInt(questions[0], 10));
+      break;
+    case '3':
+      consoleQuery(parseInt(questions[0], 10));
+      break;
+    case '4':
+      consoleQuery(parseInt(questions[0], 10));
+      break;
+    case '5':
+      consoleQuery(parseInt(questions[0], 10));
+      break;
+    case '6':
+      consoleQuery(parseInt(questions[0], 10));
+      break;
+    case '7':
+      consoleQuery(parseInt(questions[0], 10));
+      break;
+    case '8':
+      consoleQuery(parseInt(questions[0], 10));
+      break;
+    case '9':
+      consoleQuery(parseInt(questions[0], 10));
+      break;
+    case '10':
+      consoleQuery(parseInt(questions[0], 10));
+      break;
+    default:
+      createHelpOptions();
+      break;
   }
 
   connection.end();
