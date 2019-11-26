96e5d37bbfdf99978a94e753ae7f5fc3f7f39238
mehmetekinci <mehmetekinci1989@gmail.com>
2019-11-27 00:45:37 +0100

    some changes after feedback

--- Week3/_README.md
+++ Week3/_README.md
@@ -1,4 +1,4 @@
-**_ HOW WORKS APP _**
+**_HOW WORKS APP_**
 
 1- You have to use index.js file with the "npm run start" code.
 2- You have to use Postman app.
@@ -17,21 +17,21 @@ You have to use this columns in the Postman app.
 
 - OPERATIONS
 
-POST ----- /user --------------- Creates a new user.
-GET ------ /user --------------- Shows all users.
-GET ------ /user/:id ----------- Shows specific user by id.
-PUT ------ /user/:id ----------- Updates specific user by id.
-DELETE---- /user/:id ----------- Deletes specific user by id.
-
-POST ----- /todos -------------- Creates a new todo.
-GET ------ /todos -------------- Shows all todo list.
-GET ------ /todos/:id ---------- Shows specific todo by id.
-GET ------ /todos/items/:id ---- Shows items from specific todo by id.
-PUT ------ /todos/:id ---------- Updates specific user by id.
-DELETE---- /todos/:id ---------- Deletes specific user by id.
-
-POST ----- /user --------------- Creates a new item in todo list.
-GET ------ /user --------------- Shows all items.
-GET ------ /user/:id ----------- Shows specific item by id.
-PUT ------ /user/:id ----------- Updates specific item by id.
-DELETE---- /user/:id ----------- Deletes specific item by id.
+| request | endpoint         | description                           |
+| ------- | ---------------- | ------------------------------------- |
+| POST    | /user            | Creates a new user.                   |
+| GET     | /user            | Shows all users.                      |
+| GET     | /user/:id        | Shows specific user by id.            |
+| PUT     | /user/:id        | Updates specific user by id.          |
+| DELETE  | /user/:id        | Deletes specific user by id.          |
+| POST    | /todos           | Creates a new todo.                   |
+| GET     | /todos           | Shows all todo list.                  |
+| GET     | /todos/:id       | Shows specific todo by id.            |
+| GET     | /todos/items/:id | Shows items from specific todo by id. |
+| PUT     | /todos/:id       | Updates specific user by id.          |
+| DELETE  | /todos/:id       | Deletes specific user by id.          |
+| POST    | /items           | Creates a new item in todo list.      |
+| GET     | /items           | Shows all items.                      |
+| GET     | /items/:id       | Shows specific item by id.            |
+| PUT     | /items/:id       | Updates specific item by id.          |
+| DELETE  | /items/:id       | Deletes specific item by id.          |
--- Week3/app.js
+++ Week3/app.js
@@ -18,6 +18,69 @@ connection.connect(err => {
   }
 });
 
+async function checkUser(id) {
+  const selectedUserQuery = `select * from users where user_id = ?`;
+  const [selectedUser] = await execQuery(selectedUserQuery, [id]);
+
+  if (!selectedUser) {
+    const error = new Error();
+    error.code = 404;
+    error.message = `The user with id ${id} is not exists!`;
+    throw error;
+  }
+}
+
+function catchUserError(error, response) {
+  console.log(error);
+  if (error.code === 404) {
+    return response.status(error.code).send(error.message);
+  }
+
+  response.status(500).send('Unexpected server error!');
+}
+
+async function checkTodo(id) {
+  const selectedTodoQuery = `select * from todoList where todo_id = ?`;
+  const [selectedTodo] = await execQuery(selectedTodoQuery, [id]);
+
+  if (!selectedTodo) {
+    const error = new Error();
+    error.code = 404;
+    error.message = `The todo with id ${id} is not exists!`;
+    throw error;
+  }
+}
+
+function catchTodoError(error, response) {
+  console.log(error);
+  if (error.code === 404) {
+    return response.status(error.code).send(error.message);
+  }
+
+  response.status(500).send('Unexpected server error!');
+}
+
+async function checkItem(id) {
+  const selectedItemQuery = `select * from todoItems where id = ?`;
+  const [selectedItem] = await execQuery(selectedItemQuery, [id]);
+
+  if (!selectedItem) {
+    const error = new Error();
+    error.code = 404;
+    error.message = `The item with id ${id} is not exists!`;
+    throw error;
+  }
+}
+
+function catchItemError(error, response) {
+  console.log(error);
+  if (error.code === 404) {
+    return response.status(error.code).send(error.message);
+  }
+
+  response.status(500).send('Unexpected server error!');
+}
+
 async function createUser(request, response) {
   const { user_name, email, gender } = request.body;
 
@@ -27,6 +90,7 @@ async function createUser(request, response) {
     response.send(insert);
   } catch (error) {
     console.log(error);
+    response.status(500).send('Unexpected server error!');
   }
 }
 
@@ -37,33 +101,40 @@ async function getUserList(request, response) {
     response.send(getList);
   } catch (error) {
     console.log(error);
+    response.status(500).send('Unexpected server error!');
   }
 }
 
 async function getOneUser(request, response) {
-  const userId = request.params.id;
   try {
+    const userId = request.params.id;
+
+    await checkUser(userId);
+
     const getOneUserQuery = `select * from users where user_id = ?`;
     const getOneUser = await execQuery(getOneUserQuery, [userId]);
     response.send(getOneUser);
     response.end();
   } catch (error) {
-    console.log(error);
+    catchUserError(error, response);
   }
 }
 
 async function updateUser(request, response) {
-  const selectedUserQuery = `select * from users where user_id = ?`;
-  const [selectedUser] = await execQuery(selectedUserQuery, [
-    request.params.id,
-  ]);
-
-  const userId = request.params.id;
-  const userName = request.body.user_name || selectedUser.user_name;
-  const email = request.body.email || selectedUser.email;
-  const gender = request.body.gender || selectedUser.gender;
-
   try {
+    const selectedUserQuery = `select * from users where user_id = ?`;
+    const [selectedUser] = await execQuery(selectedUserQuery, [
+      request.params.id,
+    ]);
+
+    const userId = request.params.id;
+
+    await checkUser(userId);
+
+    const userName = request.body.user_name || selectedUser.user_name;
+    const email = request.body.email || selectedUser.email;
+    const gender = request.body.gender || selectedUser.gender;
+
     const updateQuery = `update users set user_name=? , email = ?, gender= ? where user_id= ?`;
     const updated = await execQuery(updateQuery, [
       userName,
@@ -74,19 +145,22 @@ async function updateUser(request, response) {
     response.send(updated);
     response.end();
   } catch (error) {
-    console.log(error);
+    catchUserError(error, response);
   }
 }
 
 async function deleteUser(request, response) {
-  const userId = request.params.id;
   try {
+    const userId = request.params.id;
+
+    await checkUser(userId);
+
     const deleteQuery = `delete from users where user_id= ?`;
-    const deleted = await execQuery(deleteQuery, [userId]);
-    response.send(deleted);
+    await execQuery(deleteQuery, [userId]);
+    response.status(200).send('This user has been deleted!');
     response.end();
   } catch (error) {
-    console.log(error);
+    catchUserError(error, response);
   }
 }
 
@@ -118,43 +192,52 @@ async function getTodoList(request, response) {
 }
 
 async function getOneTodo(request, response) {
-  const todoId = request.params.id;
   try {
+    const todoId = request.params.id;
+
+    await checkTodo(todoId);
+
     const getOneTodoQuery = `select * from todoList where todo_id = ?`;
     const getOneTodo = await execQuery(getOneTodoQuery, [todoId]);
     response.send(getOneTodo);
     response.end();
   } catch (error) {
-    console.log(error);
+    catchTodoError(error, response);
   }
 }
 
 async function getOneTodoListItems(request, response) {
-  const todoId = request.params.id;
   try {
+    const todoId = request.params.id;
+
+    await checkTodo(todoId);
+
     const getOneTodoItemsQuery = `select todo_name,todoItems.description as TodoDetails,todoItems.is_done as isDone 
     from todoList join todoItems on todoList.user_id = todoItems.user_id where todoList.todo_id=?;`;
     const getOneTodoItems = await execQuery(getOneTodoItemsQuery, [todoId]);
     response.send(getOneTodoItems);
     response.end();
   } catch (error) {
-    console.log(error);
+    catchTodoError(error, response);
   }
 }
 
 async function updateTodo(request, response) {
-  const selectedTodoQuery = `select * from todoList where todo_id = ?`;
-  const [selectedTodo] = await execQuery(selectedTodoQuery, [
-    request.params.id,
-  ]);
-
-  const todoId = request.params.id;
-  const todoName = request.body.todo_name || selectedTodo.todo_name;
-  const startDate = request.body.start_date || selectedTodo.start_date;
-  const expireDate = request.body.expire_date || selectedTodo.expire_date;
-  const userId = request.body.user_id || selectedTodo.user_id;
-
   try {
+    const selectedTodoQuery = `select * from todoList where todo_id = ?`;
+    const [selectedTodo] = await execQuery(selectedTodoQuery, [
+      request.params.id,
+    ]);
+
+    const todoId = request.params.id;
+
+    await checkTodo(todoId);
+
+    const todoName = request.body.todo_name || selectedTodo.todo_name;
+    const startDate = request.body.start_date || selectedTodo.start_date;
+    const expireDate = request.body.expire_date || selectedTodo.expire_date;
+    const userId = request.body.user_id || selectedTodo.user_id;
+
     const updateQuery = `update todoList set todo_name=? , start_date = ?, expire_date = ?, user_id = ? where todo_id= ?`;
     const updated = await execQuery(updateQuery, [
       todoName,
@@ -166,19 +249,22 @@ async function updateTodo(request, response) {
     response.send(updated);
     response.end();
   } catch (error) {
-    console.log(error);
+    catchTodoError(error, response);
   }
 }
 
 async function deleteTodo(request, response) {
-  const todoId = request.params.id;
   try {
+    const todoId = request.params.id;
+
+    await checkTodo(todoId);
+
     const deleteQuery = `delete from todoList where todo_id= ?`;
     const deleted = await execQuery(deleteQuery, [todoId]);
     response.send(deleted);
     response.end();
   } catch (error) {
-    console.log(error);
+    catchTodoError(error, response);
   }
 }
 
@@ -210,30 +296,36 @@ async function getTodoItemList(request, response) {
 }
 
 async function getOneTodoItem(request, response) {
-  const ItemId = request.params.id;
   try {
+    const itemId = request.params.id;
+
+    await checkItem(itemId);
+
     const getOneItemQuery = `select * from todoItems where id = ?`;
-    const getOneItem = await execQuery(getOneItemQuery, [ItemId]);
+    const getOneItem = await execQuery(getOneItemQuery, [itemId]);
     response.send(getOneItem);
     response.end();
   } catch (error) {
-    console.log(error);
+    catchItemError(error, response);
   }
 }
 
 async function updateTodoItem(request, response) {
-  const selectedItemQuery = `select * from todoItems where id = ?`;
-  const [selectedItem] = await execQuery(selectedItemQuery, [
-    request.params.id,
-  ]);
-
-  const itemId = request.params.id;
-  const description = request.body.description || selectedItem.description;
-  const isDone = request.body.is_done || selectedItem.is_done;
-  const todoId = request.body.todo_id || selectedItem.todo_id;
-  const userId = request.body.user_id || selectedItem.user_id;
-
   try {
+    const selectedItemQuery = `select * from todoItems where id = ?`;
+    const [selectedItem] = await execQuery(selectedItemQuery, [
+      request.params.id,
+    ]);
+
+    const itemId = request.params.id;
+
+    await checkItem(itemId);
+
+    const description = request.body.description || selectedItem.description;
+    const isDone = request.body.is_done || selectedItem.is_done;
+    const todoId = request.body.todo_id || selectedItem.todo_id;
+    const userId = request.body.user_id || selectedItem.user_id;
+
     const updateQuery = `update todoItems set description=?, is_done= ?, todo_id= ?, user_id = ? where id= ?`;
     const updated = await execQuery(updateQuery, [
       description,
@@ -245,19 +337,22 @@ async function updateTodoItem(request, response) {
     response.send(updated);
     response.end();
   } catch (error) {
-    console.log(error);
+    catchItemError(error, response);
   }
 }
 
 async function deleteTodoItem(request, response) {
-  const itemId = request.params.id;
   try {
+    const itemId = request.params.id;
+
+    await checkItem(itemId);
+
     const deleteQuery = `delete from todoItems where id= ?`;
     const deleted = await execQuery(deleteQuery, [itemId]);
     response.send(deleted);
     response.end();
   } catch (error) {
-    console.log(error);
+    catchItemError(error, response);
   }
 
   connection.end(err => {
