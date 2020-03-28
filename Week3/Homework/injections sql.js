34b44152e4bec5abb14081a979f75d663d63ed4c
Ammar <zarosho@gmail.com>
2020-03-28 09:31:28 +0100

    all is done

--- Week3/Homework/injections sql.js
+++ Week3/Homework/injections sql.js
@@ -21,9 +21,9 @@ If you use '' OR 1=1 OR '' as a name you'd get all the info.
 //Rewrite the function so that it is no longer vulnerable to SQL injection
 function getPopulation(Country, name, code, cb) {
     let passedName = name; 
+    const query = `SELECT Population FROM ${Country} WHERE Name = ? `; 
     // assuming that connection to the database is established and stored as conn
-    conn.query(
-      `SELECT Population FROM ${Country} WHERE Name = '${name}' and code = ${code}`,
+    conn.query(query, passedName,
       function(err, result) {
         if (err) cb(err);
         if (result.length == 0) cb(new Error("Not found"));
