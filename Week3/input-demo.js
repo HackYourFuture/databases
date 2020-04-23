dd1f2804c4b02cc751f38a35370314b62084c5b7
unmeshvrije <unmesh.joshi126@gmail.com>
2020-04-23 16:30:20 +0200

    add input demo

---
+++ Week3/input-demo.js
@@ -0,0 +1,13 @@
+var prompt = require('prompt');
+
+// Start the prompt
+prompt.start();
+
+// Get two properties from the user: username and email
+prompt.get(['username', 'email'], function (err, result) {
+    // Log the results.
+    console.log('Command-line input received:');
+    console.log('  username: ' + result.username);
+    console.log('  email: ' + result.email);
+    }
+    );
