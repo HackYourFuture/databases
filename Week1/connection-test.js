e45a81815c5f03fa5e88aa69e38f9e4572e778da
Fatma Hameed <fatma78hameed@gmail.com>
2020-10-15 04:26:17 -0700

    final commit, deleting package.json

--- Week1/connection-test.js
+++ Week1/connection-test.js
@@ -4,6 +4,7 @@ var connection = mysql.createConnection({
   user     : 'hyfuser',
   password : 'hyfpassword',
   database : 'userdb',
+  
   // port : xxxx // Uncomment this line and replace xxxx with the selected port number if you are not using default 3306. I also suggest to download MySQL version 5.7 because recent versions has authentication problems
 
 });
