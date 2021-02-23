4e9f2b7c73ecc856c9f5e25cc34e4e6929e2f8eb
Tarek <2b3zab666@gmail.com>
2021-02-23 15:11:23 +0100

    week1 database homework

---
+++ Week1/exercise1/index.js
@@ -0,0 +1,75 @@
+const mysql = require("mysql");
+const db = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "tarek123321",
+});
+
+let deleteTableifExist = "DROP DATABASE IF EXISTS meetup";
+let createDataBase = "CREATE DATABASE meetup;";
+
+// deleting the database if it's exist in order to be able  create it again without having errors
+db.query(deleteTableifExist, (err, results) => {
+    if (err) {
+        console.log("faild to delete database meetup", err);
+    }
+    console.log("database meetup deleted!");
+});
+
+// creating the database meetup !
+db.query(createDataBase, (err, results) => {
+    if (err) {
+        console.log("faild to create database meetup", err);
+    }
+    console.log("database meetup Created!!", results);
+});
+
+// creating creatingTablesCommands variables which will holds our sql commands for using the right database , creating tables invitee ,room ,meeting.
+let creatingTablesCommands = [
+    "USE meetup",
+    "CREATE TABLE Invitee ( invitee_no INT NOT NULL  AUTO_INCREMENT, invitee_name VARCHAR(100) , invited_by VARCHAR(100) , PRIMARY KEY(invitee_no) )",
+    "CREATE TABLE Room ( room_no INT NOT NULL  AUTO_INCREMENT, room_name VARCHAR(100) , floor_number INT , PRIMARY KEY(room_no) )",
+    "CREATE TABLE Meeting ( meeting_no INT NOT NULL  AUTO_INCREMENT, meeting_title VARCHAR(100) , starting_time  DATETIME , ending_time DATETIME , room_no INT, PRIMARY KEY(meeting_no))",
+];
+
+// function to create Queries  from the array
+const createQueries = function (array) {
+    array.forEach((query) => {
+        db.query(query, (err, results) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log("Table Created!!");
+        });
+    });
+};
+
+// function to insert  Queries from the array
+const insertQueries = function (array) {
+    array.forEach((query) => {
+        db.query(query, (err, results) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log("Data Inserted");
+        });
+    });
+};
+
+// passing creatingTablesCommands array to our function, to use the database meetup, then creating tables invitte,room,meeting.
+createQueries(creatingTablesCommands);
+
+// creating insertingDataCommands variables which will holds our sql commands for inserting data to the three tables
+let insertingDataCommands = [
+    'INSERT INTO Invitee (invitee_name , invited_by) VALUES ( "Tarek" , "Tom" ),( "Mark" , "Anne" ),( "Sami" , "Sara" ),( "Rob" , "Thomas" ),( "lana" , "Johan" )',
+    'INSERT INTO Room (room_name , floor_number) VALUES ( "Local 1" , "4" ),( "Local 22" ,"4" ),( "living-room","1" ),( "meeting-room","11" ),( "hidden-room","13" )',
+    'INSERT INTO Meeting (meeting_title , starting_time , ending_time , room_no ) VALUES ( "Information about Corona" , "2022-01-01 14:00:00" , "2022-01-01 16:00:00" , "2" ),( "How to find a job" , "2025-02-01 14:00:00" , "2025-02-01 16:00:00" , "1"  ),( "Timing", "2025-02-01 17:00:00" , "2025-02-01 19:00:00" , "5" ),( "Pansion","2025-02-01 17:00:00" , "2025-02-01 19:00:00" , "11"  ),( "How to deal with your childeren","2025-02-01 17:00:00" , "2025-02-01 19:00:00" , "11" )',
+];
+
+// passing insertingDataCommands array to our function, to insert 5 rows in each tables.
+insertQueries(insertingDataCommands);
+
+db.end((err) => {
+    console.log("connection ended");
+});
---
+++ Week1/exercise2/index.js
@@ -0,0 +1,57 @@
+const mysql = require("mysql");
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "tarek123321",
+    database: "world",
+});
+
+// using the database world
+db.query("USE world;", (err, results) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log("database wolrd used");
+});
+
+// function to query the data from data base using query parameter, to answer the quiestion parameter .
+const queringAnswersFromDb = function (queriesList, question) {
+    queriesList.forEach((query, index) => {
+        db.query(query, (err, results) => {
+            console.log(question[index]);
+            console.log("Answer is:", results);
+        });
+    });
+};
+
+const listOfQueries = [
+    "SELECT Name FROM world.country WHERE Population > 8000000 ; ",
+    " SELECT Name FROM world.country WHERE Name LIKE '%land%' ",
+    "SELECT Name FROM world.country WHERE Population BETWEEN 500000 AND 1000000",
+    "SELECT Name FROM world.country WHERE Continent = 'Europe' ",
+    " SELECT Name AS Names_Of_Largest_Cities FROM world.country ORDER BY SurfaceArea DESC ",
+    " SELECT Name as DutchCities FROM world.city WHERE CountryCode = 'NLD' ",
+    " SELECT Population as PopulationOfRotterdam FROM world.city WHERE Name = 'Rotterdam' ",
+    " SELECT Name AS BiggestCountries , SurfaceArea FROM world.country ORDER BY SurfaceArea DESC LIMIT 10;",
+    " SELECT Name FROM world.city ORDER BY Population DESC LIMIT 10; ",
+    " SELECT SUM(Population) AS World_Population FROM world.country;",
+];
+
+const listOfQuestions = [
+    "What are the names of countries with population greater than 8 million?",
+    "What are the names of countries that have “land” in their names?",
+    "What are the names of the cities with population in between 500,000 and 1 million?",
+    "What's the name of all the countries on the continent ‘Europe’?",
+    "List all the countries in the descending order of their surface areas.",
+    "What are the names of all the cities in the Netherlands?",
+    "What is the population of Rotterdam?",
+    "What's the top 10 countries by Surface Area?",
+    "What's the top 10 most populated cities?",
+    "What is the population number of the world?",
+];
+
+queringAnswersFromDb(listOfQueries, listOfQuestions);
+
+db.end((err) => {
+    console.log("connection ended");
+});
