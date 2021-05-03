19f67bd23bc1f2f07c66c299d8c88101b4f1daf6
Burak <isikburak142@gmail.com>
2021-05-04 01:49:18 +0200

    Made more readeble for developers

--- Week1/homework/index.js
+++ Week1/homework/index.js
@@ -26,63 +26,69 @@ const deleteDb = "DROP DATABASE IF EXISTS meetup";
 const createDb = "CREATE DATABASE meetup";
 const useDb = "USE meetup";
 
-
 addQuery(deleteDb, `Deleted.`);
 addQuery(createDb, `Database is created.`);
 addQuery(useDb, `Connected.`);
 
+// Create tables
 const queries = [
   "CREATE TABLE Invitee (invitee_no INT AUTO_INCREMENT PRIMARY KEY, invitee_name  VARCHAR(50), invited_by  VARCHAR(50))",
   "CREATE TABLE Room (room_no INT AUTO_INCREMENT PRIMARY KEY, room_name   VARCHAR(50), floor_number  INT)",
   "CREATE TABLE Meeting (meeting_no INT AUTO_INCREMENT PRIMARY KEY, meeting_title   VARCHAR(50), starting_time  TIME, ending_time TIME, room_no INT)",
 ];
-
-// Create tables
 queries.forEach((query) => addQuery(query, `Table is created`));
 
+// Insert Values
+const insertQueries = [
+  {
+    statement: "INSERT INTO Invitee  (invitee_name,invited_by) VALUES ?",
+    values: [
+      ["Burak", "Adam"],
+      ["Bob", "Sofia"],
+      ["Johnny", "Will"],
+      ["Sara", "George"],
+      ["Lisa", "Kyle"],
+    ],
+    message: "",
+  },
+  {
+    statement: "INSERT INTO Room     (room_name,floor_number)  VALUES ?",
+    values: [
+      ["First room", 2],
+      ["Second room", 4],
+      ["Third room", 6],
+      ["Fourth room", 8],
+      ["Fifth room", 10],
+    ],
+    message: "",
+  },
+  {
+    statement:
+      "INSERT INTO Meeting  (meeting_title,starting_time,ending_time, room_no)  VALUES ?",
+    values: [
+      ["First meeting", "08:00:00", "10:00:00", 1],
+      ["Second meeting", "10:00:00", "12:00:00", 2],
+      ["Third meeting", "12:00:00", "14:00:00", 3],
+      ["Fourth meeting", "14:00:00", "16:00:00", 4],
+      ["Fifth meeting", "16:00:00", "18:00:00", 5],
+    ],
+    message: "",
+  },
+];
 
-// Inserting values into tables
-const insertQuery = (query, post, msg) => {
-  connection.query(query, post, (err) => {
+function insertQuery(insertQueries, msg) {
+  connection.query(insertQueries.statement, [insertQueries.values], (err) => {
     if (err) throw err;
     console.log(msg);
   });
-};
-
-const valuesQueries = [
-  `INSERT INTO Invitee  (invitee_name,invited_by) VALUES ?`,
-  `INSERT INTO Room     (room_name,floor_number)  VALUES ?`,
-  `INSERT INTO Meeting  (meeting_title,starting_time,ending_time, room_no)  VALUES ?`,
-];
-
-const inviteeValues = [
-  ["John", "Mary"],
-  ["Bob", "Sofia"],
-  ["Johnny", "Will"],
-  ["Sara", "George"],
-  ["Lisa", "Kyle"],
-];
-
-let roomValues = [
-  ["First room", 1],
-  ["Second room", 2],
-  ["Third room", 3],
-  ["Fourth room", 4],
-  ["Fifth room", 5],
-];
-
-const meetingValues = [
-  ["First meeting", "08:00:00", "10:00:00", 1],
-  ["Second meeting", "10:00:00", "12:00:00", 2],
-  ["Third meeting", "12:00:00", "14:00:00", 3],
-  ["Fourth meeting", "14:00:00", "16:00:00", 4],
-  ["Fifth meeting", "16:00:00", "18:00:00", 5],
-];
-
-const allValues = [inviteeValues, roomValues, meetingValues ];
-
-for (let i = 0; i < allValues.length; i++) {
-  insertQuery(valuesQueries[i], [allValues[i]], `Inserted`);
 }
+
+function addMessage(insertQuery, index) {
+  return { ...insertQuery, message: `Hi Burak ${index}!` };
+}
+
+insertQueries.forEach((query) => insertQuery(query, `Values are inserted`));
+
+insertQueries.map(addMessage);
 
 connection.end();
