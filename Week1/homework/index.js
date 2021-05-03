8cc997ff5181576d05c717d6056b33fcbf461309
Burak <isikburak142@gmail.com>
2021-05-03 03:42:34 +0200

    I changed the password

--- Week1/homework/index.js
+++ Week1/homework/index.js
@@ -4,7 +4,7 @@ const mysql = require("mysql");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Giresun3428@",
+  password: "hfypassword",
 });
 
 // Connect
