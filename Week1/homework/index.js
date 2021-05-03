d590fe92fda04e5f9ec32df53ac4c67f87f241f4
Burak <isikburak142@gmail.com>
2021-05-03 03:43:44 +0200

    Reset password as hfypassword

--- Week1/homework/index.js
+++ Week1/homework/index.js
@@ -4,7 +4,7 @@ const mysql = require("mysql");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "hfypassword",
+  password: "hyfpassword",
 });
 
 // Connect
