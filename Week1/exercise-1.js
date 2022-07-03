e6394f032c490669f439b9fc9d1a376f78988110
Fadi <fady-naddaf@hotmail.com>
2022-07-03 11:04:26 +0200

    Editing the Homework week1

--- Week1/exercise-1.js
+++ Week1/exercise-1.js
@@ -15,23 +15,24 @@ const createDatabase = 'CREATE DATABASE meetup;';
 const useDatabase = 'USE meetup;';
 
 const createInviteeTable = `CREATE TABLE Invitee(
-  invitee_no INT, 
+  invitee_no INT UNIQUE NOT NULL PRIMARY KEY, 
   invitee_name VARCHAR(50) NOT NULL, 
   invited_by VARCHAR(50) NOT NULL
 );`;
 
 const createRoomTable = `CREATE TABLE Room(
-  room_no INT, 
+  room_no INT UNIQUE NOT NULL PRIMARY KEY, 
   room_name VARCHAR(50) NOT NULL, 
   floor_number int
 );`;
 
 const createMeetingTable = `CREATE TABLE Meeting(
-  meeting_no INT, 
+  meeting_no INT UNIQUE NOT NULL PRIMARY KEY, 
+  invitee_no INT NOT NULL,
   meeting_title VARCHAR(100) NOT NULL, 
   starting_time DATETIME, 
   ending_time DATETIME, 
-  room_no INT
+  room_no INT NOT NULL
 );`;
 
 const insertInviteeTable = `INSERT INTO invitee (invitee_no, invitee_name, invited_by)
@@ -41,12 +42,12 @@ const insertInviteeTable = `INSERT INTO invitee (invitee_no, invitee_name, invit
   (3, 'Ali', 'Wouter'),
   (4, 'Yasemin', 'Jonathan');`;
 
-const insertMeetingTable = `INSERT INTO Meeting (meeting_no, meeting_title, starting_time, ending_time, room_no)
+const insertMeetingTable = `INSERT INTO Meeting (meeting_no, invitee_no, meeting_title, starting_time, ending_time, room_no)
   VALUES
-  (1, 'database', '2022-06-30 11:00:00', '2022-06-30 14:00:00', 105),
-  (2, 'Browser', '2022-07-01 11:00:00', '2022-07-01 14:00:00', 101),
-  (3, 'Node.js', '2022-06-30 14:30:00', '2022-06-30 16:30:00', 103),
-  (4, 'Browser', '2022-07-01 11:00:00', '2022-07-01 14:00:00', 102);`;
+  (111, 1, 'database', '2022-06-30 11:00:00', '2022-06-30 14:00:00', 105),
+  (112, 2, 'Browser', '2022-07-01 11:00:00', '2022-07-01 14:00:00', 101),
+  (113, 3, 'Node.js', '2022-06-30 14:30:00', '2022-06-30 16:30:00', 103),
+  (114, 4, 'Browser', '2022-07-01 11:00:00', '2022-07-01 14:00:00', 102);`;
 
 const insertRoomTable = `INSERT INTO Room (room_no, room_name, floor_number)
   VALUES
