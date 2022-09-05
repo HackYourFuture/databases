f3f386b99c7a7e5f4d6b8129ce5984eb3ce5b617
unmeshvrije <unmesh.joshi126@gmail.com>
2022-09-05 14:01:47 +0200

    add socket path in the connection test

--- Week1/connection-test.js
+++ Week1/connection-test.js
@@ -4,6 +4,12 @@ var connection = mysql.createConnection({
   user     : 'hyfuser',
   password : 'hyfpassword',
   database : 'userdb',
+  // If you get connection with
+  // errno: -61,
+  // code: 'ECONNREFUSED',
+  // Then, the following line will solve it
+  // Note that your socket file path may be different
+  socketPath: '/tmp/mysql.sock'
   
   // port : xxxx // Uncomment this line and replace xxxx with the selected port number if you are not using default 3306. I also suggest to download MySQL version 5.7 because recent versions has authentication problems
 
