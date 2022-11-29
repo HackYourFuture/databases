2520e4fbf7662b8040a20bc94f07a0764439205f
Hikmet Dag <temkihgad@gmail.com>
2022-11-29 13:08:35 +0100

    Hikmet-Database-Week1

---
+++ Week1/homework/data.js
@@ -0,0 +1,45 @@
+import { createConnection } from "mysql";
+
+export const tables = [
+      `CREATE TABLE if not exists Invitee (
+          invitee_no INT PRIMARY KEY  AUTO_INCREMENT, 
+          invitee_name VARCHAR(200),
+          invited_by VARCHAR(200));`,
+
+      `CREATE TABLE if not exists Room (
+          room_no INT PRIMARY KEY  AUTO_INCREMENT, 
+          room_name TEXT, 
+          floor_number INT );`,
+
+      `CREATE TABLE if not exists Meeting (
+          meeting_no INT PRIMARY KEY  AUTO_INCREMENT,
+          meeting_title TEXT,
+          starting_time TIMESTAMP ,
+          ending_time TIMESTAMP,
+          room_no int,
+          FOREIGN KEY(room_no) REFERENCES ROOM(room_no) 
+          );`,
+];
+
+export const values = [
+      ` INSERT INTO  Invitee (invitee_name, invited_by)
+         VALUES ('Person1', 'Person2'),
+         ('Person3','Person3'),
+         ('Person 4','Person2'),
+         ('Person5','Person3'),
+         ('Person6','Person1')`,
+
+      ` INSERT INTO  Room (room_name, floor_number)
+           VALUES ('Room1', 1),
+           ('Room2',2),
+           ('Room3',3),
+           ('Room4',4),
+           ('Room5',5)`,
+
+      `INSERT INTO Meeting(meeting_title, starting_time, ending_time,room_no )
+       VALUES('Meeting Room 1', '2022-10-29 17:00:00', '2022-10-29 19:00:00',2),
+       ('Meeting Room 2', '2022-10-29 12:00:00', '2022-10-29 13:00:00',3),
+       ('Meeting Room 3', '2022-10-29 09:00:00', '2022-10-29 10:00:00',1),
+       ('Meeting Room 4', '2022-10-29 11:00:00', '2022-10-29 12:00:00',4),
+       ('Meeting Room 5', '2022-10-29 13:00:00', '2022-10-29 14:00:00',5)`,
+];
---
+++ Week1/homework/exercise1.js
@@ -0,0 +1,43 @@
+import mysql from "mysql";
+import { tables, values } from "./data.js";
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'hyfuser',
+    password: 'hyfpassword',
+    database: 'meetup'
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('connected...');
+});
+
+//Create Tables
+const createTables = (table) => {
+    connection.query(table, (err, result) => {
+        if (err) throw err;
+        console.log("table is added");
+    });
+};
+
+//Adding Values
+const addingValues = (value) => {
+    connection.query(value, (err, result) => {
+        if (err) throw err;
+        console.log("value is added");
+    });
+};
+
+const dropTables = () => {
+    const sql = `DROP TABLE Room , Meeting , Invitee`;
+  
+    connection.query(sql, (err, result) => {
+      if (err) console.log(err);
+  
+      console.log("Tables Droped");
+    });
+  };
+  dropTables();
+tables.forEach(table => createTables(table));
+values.forEach(value => addingValues(value)); 
