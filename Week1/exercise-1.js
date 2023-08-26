0004358164610037d7fbb7ff1b94ffa429e097d8
Ahmet Avci <ahmetavci.8873@gmail.com>
2023-08-26 16:46:17 +0200

    exercises-database-week1

--- Week1/exercise-1.js
+++ Week1/exercise-1.js
@@ -1,66 +1,85 @@
-const mysql = require("mysql");
+const mysql = require("mysql2/promise");
 
-const connectionConfig = {
+const dbConfig = {
   host: "localhost",
   user: "hyfuser",
   password: "hyfpassword",
 };
 
-function createDatabase(connection) {
-  connection.query("CREATE DATABASE IF NOT EXISTS meetup", (error) => {
-    if (error) console.log(error);
-    console.log("Database created: meetup");
-    connection.changeUser({ database: "meetup" }, (error) => {
-      if (error) console.log(error);
-      createTables(connection);
-    });
-  });
-}
+const createDatabaseQuery = `CREATE DATABASE IF NOT EXISTS meetup`;
+const useDatabaseQuery = `USE meetup`;
+const createInviteeTableQuery = `
+  CREATE TABLE IF NOT EXISTS Invitee (
+    invitee_no INT AUTO_INCREMENT PRIMARY KEY,
+    invitee_name VARCHAR(255),
+    invited_by INT
+  )
+`;
+const createRoomTableQuery = `
+  CREATE TABLE IF NOT EXISTS Room (
+    room_no INT AUTO_INCREMENT PRIMARY KEY,
+    room_name VARCHAR(255),
+    floor_number INT
+  )
+`;
+const createMeetingTableQuery = `
+  CREATE TABLE IF NOT EXISTS Meeting (
+    meeting_no INT AUTO_INCREMENT PRIMARY KEY,
+    meeting_title VARCHAR(255),
+    starting_time DATETIME,
+    ending_time DATETIME,
+    room_no INT,
+    FOREIGN KEY (room_no) REFERENCES Room(room_no)
+  )
+`;
+const insertDataQueries = [
+  `
+  INSERT INTO Invitee (invitee_name, invited_by) VALUES
+    ('Invitee 1', NULL),
+    ('Invitee 2', 1),
+    ('Invitee 3', 1),
+    ('Invitee 4', 2),
+    ('Invitee 5', 3)
+  `,
+  `
+  INSERT INTO Room (room_name, floor_number) VALUES
+    ('Room A', 1),
+    ('Room B', 2),
+    ('Room C', 1),
+    ('Room D', 3),
+    ('Room E', 2)
+  `,
+  `
+  INSERT INTO Meeting (meeting_title, starting_time, ending_time, room_no) VALUES
+    ('Meeting 1', '2023-08-26 10:00:00', '2023-08-26 12:00:00', 1),
+    ('Meeting 2', '2023-08-27 14:00:00', '2023-08-27 16:00:00', 2),
+    ('Meeting 3', '2023-08-28 09:30:00', '2023-08-28 11:30:00', 3),
+    ('Meeting 4', '2023-08-29 11:00:00', '2023-08-29 13:00:00', 4),
+    ('Meeting 5', '2023-08-30 15:00:00', '2023-08-30 17:00:00', 5)
+  `,
+];
 
-function createTables(connection) {
-  const createTableQueries = [
-    `CREATE TABLE IF NOT EXISTS Invitee (
-      invitee_no INT AUTO_INCREMENT PRIMARY KEY,
-      invitee_name VARCHAR(255),
-      invited_by INT
-    );`,
-    `CREATE TABLE IF NOT EXISTS Room (
-      room_no INT AUTO_INCREMENT PRIMARY KEY,
-      room_name VARCHAR(255),
-      floor_number INT
-    );`,
-    `CREATE TABLE IF NOT EXISTS Meeting (
-      meeting_no INT AUTO_INCREMENT PRIMARY KEY,
-      meeting_title VARCHAR(255),
-      starting_time DATETIME,
-      ending_time DATETIME,
-      room_no INT,
-      FOREIGN KEY (room_no) REFERENCES Room(room_no)
-    );`,
-  ];
+async function setupDatabase() {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
 
-  let queryIndex = 0;
+    await connection.query(createDatabaseQuery);
+    await connection.query(useDatabaseQuery);
 
-  function createNextTable() {
-    if (queryIndex < createTableQueries.length) {
-      connection.query(createTableQueries[queryIndex], (error) => {
-        if (error) console.log(error);
-        queryIndex++;
-        createNextTable();
-      });
-    } else {
-      console.log("Tables created: Invitee, Room, Meeting");
-      insertData(connection);
+    await connection.query(createInviteeTableQuery);
+    await connection.query(createRoomTableQuery);
+    await connection.query(createMeetingTableQuery);
+
+    for (const query of insertDataQueries) {
+      await connection.query(query);
     }
-  }
 
-  createNextTable();
+    console.log("Database setup complete.");
+
+    connection.end();
+  } catch (error) {
+    console.error("Error setting up the database:", error);
+  }
 }
 
-const connection = mysql.createConnection(connectionConfig);
-
-connection.connect((error) => {
-  if (error) console.log(error);
-  console.log("Connected to MySQL server");
-  createDatabase(connection);
-});
+setupDatabase();
--- Week1/query-world-db.js
+++ Week1/query-world-db.js
@@ -1,114 +1,39 @@
-const mysql = require("mysql");
+const mysql = require("mysql2/promise");
 
-const connectionConfig = {
+const dbConfig = {
   host: "localhost",
   user: "hyfuser",
   password: "hyfpassword",
   database: "world",
 };
 
-const connection = mysql.createConnection(connectionConfig);
+const queries = [
+  "SELECT Name FROM country WHERE Population > 8000000",
+  "SELECT Name FROM country WHERE Name LIKE '%land%'",
+  "SELECT Name FROM city WHERE Population BETWEEN 500000 AND 1000000",
+  "SELECT Name FROM country WHERE Continent = 'Europe'",
+  "SELECT Name FROM country ORDER BY SurfaceArea DESC",
+  "SELECT Name FROM city WHERE CountryCode = 'NLD'",
+  "SELECT Population FROM city WHERE Name = 'Rotterdam'",
+  "SELECT Name FROM country ORDER BY SurfaceArea DESC LIMIT 10",
+  "SELECT Name FROM city ORDER BY Population DESC LIMIT 10",
+  "SELECT SUM(Population) AS total_population FROM country",
+];
 
-connection.connect((error) => {
-  if (error) throw error;
-  console.log("Connected to MySQL server");
-  executeQueries();
-});
+async function runQueries() {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
 
-function executeQueries() {
-  connection.query(
-    `SELECT Name FROM country WHERE Population > 8000000`,
-    (error, results) => {
-      if (error) throw error;
-      console.log("Countries with population greater than 8 million:");
-      console.log(results);
+    for (const query of queries) {
+      const [results] = await connection.query(query);
+      console.log(JSON.stringify(results, null, 2));
+      console.log("--------------------------------------");
     }
-  );
 
-  connection.query(
-    `SELECT Name FROM country WHERE Name LIKE '%land%'`,
-    (error, results) => {
-      if (error) throw error;
-      console.log('Countries with "land" in their names:');
-      console.log(results);
-    }
-  );
-
-  connection.query(
-    `SELECT Name FROM city WHERE Population BETWEEN 500000 AND 1000000`,
-    (error, results) => {
-      if (error) throw error;
-      console.log("Cities with population between 500,000 and 1 million:");
-      console.log(results);
-    }
-  );
-
-  connection.query(
-    `SELECT Name FROM country WHERE Continent = 'Europe'`,
-    (error, results) => {
-      if (error) throw error;
-      console.log("Countries in the continent Europe:");
-      console.log(results);
-    }
-  );
-
-  connection.query(
-    `SELECT Name FROM country ORDER BY SurfaceArea DESC`,
-    (error, results) => {
-      if (error) throw error;
-      console.log("Countries in descending order of their surface areas:");
-      console.log(results);
-    }
-  );
-
-  connection.query(
-    `SELECT Name FROM city WHERE CountryCode = 'NLD'`,
-    (error, results) => {
-      if (error) throw error;
-      console.log("Cities in the Netherlands:");
-      console.log(results);
-    }
-  );
-
-  connection.query(
-    `SELECT Population FROM city WHERE Name = 'Rotterdam'`,
-    (error, results) => {
-      if (error) throw error;
-      console.log("Population of Rotterdam:");
-      console.log(results);
-    }
-  );
-
-  connection.query(
-    `SELECT Name, SurfaceArea FROM country ORDER BY SurfaceArea DESC LIMIT 10`,
-    (error, results) => {
-      if (error) throw error;
-      console.log("Top 10 countries by Surface Area:");
-      console.log(results);
-    }
-  );
-
-  connection.query(
-    `SELECT Name, Population FROM city ORDER BY Population DESC LIMIT 10`,
-    (error, results) => {
-      if (error) throw error;
-      console.log("Top 10 most populated cities:");
-      console.log(results);
-    }
-  );
-
-  connection.query(
-    `SELECT SUM(Population) AS WorldPopulation FROM country`,
-    (error, results) => {
-      if (error) throw error;
-      console.log("Population number of the world:");
-      console.log(results);
-    }
-  );
-
-  closeConnection();
+    connection.end();
+  } catch (error) {
+    console.error("Error executing queries:", error);
+  }
 }
 
-function closeConnection() {
-  connection.end();
-}
+runQueries();
