08e4b3eec41bdf72858785df8ee82e540c59faf7
Aziz <103178186+azizsigar@users.noreply.github.com>
2024-08-19 20:56:33 +0200

    sql injection is crashed

--- Week3/sql-injection.js
+++ Week3/sql-injection.js
@@ -1,46 +1,42 @@
 var prompt = require('prompt');
-var mysql      = require('mysql');
+var mysql = require('mysql');
 const util = require('util');
 
 const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'hyfuser',
-  password : 'hyfpassword',
-  database : 'company',
+  host: 'localhost',
+  user: 'hyfuser',
+  password: 'hyfpassword',
+  database: 'company',
   multipleStatements: true
 });
 
-const execQuery = util.promisify(connection.query.bind(connection))
-const input = util.promisify(prompt.get.bind(this))
+const execQuery = util.promisify(connection.query.bind(connection));
+const input = util.promisify(prompt.get.bind(this));
 
 async function queryDatabase() {
+  var input_number = "";
+  prompt.start();
+  try {
+    const result = await input(['employee_number']);
+    input_number = result.employee_number;
 
-    var input_number = ""
-    prompt.start();
-    try {
-        const result = await input(['employee_number']);
-        input_number = result.employee_number
 
-        // 1. Naive way of passing the parameter to the query
-        const select_query = `select * from employees WHERE employee_id =  ${input_number};`
+    const select_query = `SELECT * FROM employees WHERE employee_id = ?`;
 
-        // 2. Escaping the parameter ( replacing the unwanted characters)
-        //const select_query = `select * from employees WHERE employee_id =` + connection.escape(input_number);
+    connection.connect();
+    console.log(select_query);
 
-        // 3. Using a question mark syntax to do the escaping 
-        //const select_query = `select * from employees WHERE employee_id = ?`
 
-        connection.connect();
-        console.log(select_query);
-        const results = await execQuery(select_query, input_number);
-        for (r of results) {
-            console.log(r);
-        }
-    } catch(error) {
-        console.error(error);
+    const results = await execQuery(select_query, [input_number]);
+
+    for (const r of results) {
+      console.log(r);
     }
-    
+  } catch (error) {
+    console.error(error);
+  } finally {
     connection.end();
+  }
 }
 
 queryDatabase();
